c76e1d9764061275dab391a07bd00d5c87171bb9
Rohan Dvivedi <rohandvivedi@gmail.com>
2019-05-22 00:54:48 +0530

    made array, may be buggy testing incomplete

---
+++ inc/array.h
@@ -0,0 +1,45 @@
+#ifndef ARRAY_H
+#define ARRAY_H
+
+#include<stdlib.h>
+
+// the array will maintain all of its memory on its own
+// you manage your memory, array will manage its own memory on heap
+// you may pass pointers, but that will be just to read your object, promise
+
+struct array
+{
+	// can be zero
+	unsigned long long int size;
+
+	// total number of buckets, can not be 0, at least 1
+	unsigned long long int max_size;
+
+	// we maintain array of pointers each of which point to actual data
+	void** data_p_p;
+
+	// size of data
+	unsigned long long int size_of_data_element;
+
+	// this is the factor, by which the previous size be incremented
+	unsigned long long int increment_factor;
+
+	// this is the increment amount
+	unsigned long long int increment_offset;
+
+	// new_size = old_size * increment_factor + increment_offset
+};
+
+array* get_array(unsigned long long int min_size, unsigned long long int size_of_data_element);
+
+void delete_array(array* array_p);
+
+void* get_element(array* array_p,unsigned long long int i);
+
+void append_element(array* array_p, const void* data_p);
+
+void print_array(array* array_p, void (*print_element)(void*));
+
+void sort_array(array* array_p, int (*comparator_function)(void*, void*));
+
+#endif
---
+++ src/array.c
@@ -0,0 +1,91 @@
+#include<array.h>
+
+array* get_array(unsigned long long int min_size, unsigned long long int size_of_data_element)
+{
+	array* array_p = (array*) calloc(1, sizeof(array));
+	array_p->max_size = min_size;
+	array_p->size = 0;
+	array_p->size_of_data_element = size_of_data_element;
+	array_p->increment_offset = 1;
+	array_p->increment_factor = 2;
+	array_p->data_p_p = array_p->max_size > 0 ? ((void**)calloc(array_p->max_size, sizeof(void*))): NULL;
+}
+
+void delete_array(array* array_p)
+{
+	for(unsigned long long int i = 0; i < array_p->size; i++)
+	{
+		if(array_p->data_p_p[i] != NULL)
+		{
+			free(array_p->data_p_p[i]);
+		}
+	}
+	if(array_p->max_size > 0 && array_p->data_p_p != NULL)
+	{
+		free(array_p->data_p_p);
+	}
+	if(array_p != NULL)
+	{
+		free(array_p);
+	}
+}
+
+void* get_element(array* array_p, unsigned long long int i)
+{
+	if(array_p->max_size > i)
+	{
+		return ((void*)(((char*)array_p->data_p_p)[i]));
+	}
+	else
+	{
+		return NULL;
+	}
+}
+
+void append_element(array* array_p, const void* data_p)
+{
+	if(array_p->max_size < array_p->size && array_p->data_p_p != NULL)
+	{
+		// you fuck made an error
+		return ;	
+	}
+	else if(array_p->max_size == array_p->size)
+	{
+		// compute new_size
+		unsigned long long int new_max_size = ( array_p->max_size * array_p->increment_factor ) + array_p->increment_offset;
+
+		// taking precaution
+		new_max_size = new_max_size == i ? new_max_size + 1 : new_size;
+
+		// request memory for the new computed size
+		void** new_data_p_p = ((void**)calloc(array_p->max_size, sizeof(void*)));
+
+		// copy all pointers from the old pointers array
+		for(int i = 0; i < array_p->size; i++)
+		{
+			((char*)new_data_p_p[i]) = ((char*)array_p->data_p_p)[i];
+		}
+
+		// free the old pointers array
+		free(array_p->data_p_p);
+
+		// new assignment to data_p_p and the max_size
+		array_p->data_p_p = new_data_p_p;
+		array_p->max_size = new_max_size;
+	}
+	if(data_p != NULL)
+	{
+		// get pointer to to new data memory required
+		void* new_data_p = calloc(1, size_of_data_element);
+
+		// copy data from user's pointer to array's memory that we requested above
+		for(unsigned long long int i = 0; i < size_of_data_element; i++)
+		{
+			// word by word :p
+			((char*)new_data_p)[i] = ((char*)data_p)[i];
+		}
+
+		// assign a new data there and then increment the current size of the array
+		array_p->data_p_p[array_p->size++] = new_data_p;
+	}
+}
