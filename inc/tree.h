128a27d9706d4ee57f99b28d2e709c6d2bdfec7e
Rohan Dvivedi <rohandvivedi@gmail.com>
2019-05-22 17:44:58 +0530

    tree definition complete, method implkementation left

--- inc/tree.h
+++ inc/tree.h
@@ -3,19 +3,22 @@
 
 #include<array.h>
 
+// tree manages its own data
+// if tree takes a pointer from you, it will be to copy your data
+
 typedef struct tree tree;
 struct tree
 {
 	// size of data, on each node that each node of the tree holds
 	unsigned long long int size_of_data_on_node;
 
-	// total number of buckets, can not be 0, at least 1
+	// total number of children buckets/capacity at initialization
 	unsigned long long int children_default_size;
 
-	// this is the factor, by which the size of node children increases
+	// this is the factor, by which the size of children of any node increases
 	unsigned long long int increment_factor;
 
-	// this is the increment amount, by which the size of node children increases
+	// this is the increment amount, by which the size of node children increases, over the factor
 	unsigned long long int increment_offset;
 
 	// this is the root node of the tree
@@ -28,22 +31,49 @@ struct tree
 typedef struct node node;
 struct node
 {
+	// pointer to the parent node
+	// parent of root node is NULL
+	node* parent;
+
 	// this is pointer to the data that the node will hold
 	void* data_p;
 
 	// can be zero
-	unsigned long long int children_size;
+	unsigned long long int children_occupied_size;
 
 	// total number of buckets
-	unsigned long long int children_max_size;
+	unsigned long long int children_total_size;
 
 	// children nodes
 	node** children;
 };
 
+// builds a tree with no nodes
 tree* get_tree(unsigned long long int children_default_size, unsigned long long int size_of_data_element_on_node);
 
+// gives a node with data, copied to itself from data_p
+node* get_node(tree* tree, const void* data_p);
+
+// adds a child to parent node, as per the description of the characteristics of tree_p
+void* add_child(tree* tree ,node* parent_p);
+
+// builds a bond between parent and a child
+void connect(tree* tree, node* parent_p, node* child_p);
+
+// deletes all of the tree
+void delete_tree(tree* tree)
+
+// deletes all its children aswell
+// recursive
+void delete_node(node* node)
+
+// tells us if the node is a leaf node
 int is_leaf(node* node_p);
 
+// tells us if the node is a root node
+int is_root(node* node_p);
+
+// tells us if the node is a root node
+int is_root(node* node_p);
 
 #endif
