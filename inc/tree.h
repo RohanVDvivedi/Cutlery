029b2a3745da37ca29a9d5cebe836bdfe811f86e
Rohan Dvivedi <rohandvivedi@gmail.com>
2019-05-22 09:29:29 +0530

    inccomplkete tree

---
+++ inc/tree.h
@@ -0,0 +1,23 @@
+#ifndef TREE_H
+#define TREE_H
+
+#include<array.h>
+
+typedef struct tree tree;
+struct tree
+{
+
+
+
+};
+
+typedef struct node node;
+struct node
+{
+	unsigned long long int size_of_data_element;
+	void* data
+	array_p* children_nodes;
+};
+
+
+#endif
