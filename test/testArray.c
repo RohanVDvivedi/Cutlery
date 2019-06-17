2e0b10bf2efedb866d20cb304ddeb764ecfba01d
Rohan Dvivedi <rohandvivedi@gmail.com>
2019-06-17 22:22:53 +0200

    wrote a faulty test src

--- test/testArray.c
+++ test/testArray.c
@@ -7,10 +7,53 @@ struct teststruct
 	char* s;
 };
 
+void print_ts(const void* tsv)
+{
+	printf(" %d, %s", ((ts*)tsv)->a, ((ts*)tsv)->s);
+}
+
 int main()
 {
-	array_p* arr = get_array(3, sizeof(ts));
+	array* array_p = get_array(3, sizeof(ts));
 
-	delete_array(arr);
+	print_array(array_p, print_ts);
+
+	set_element(array_p, &((ts){111, "def"}), 1);
+
+	print_array(array_p, print_ts);
+
+	expand_array(array_p);
+
+	print_array(array_p, print_ts);
+
+	set_element(array_p, &((ts){333, "jkl"}), 3);
+	set_element(array_p, &((ts){222, "ghi"}), 2);
+
+	print_array(array_p, print_ts);
+
+	set_element(array_p, &((ts){555, "one 0 3"}), 5);
+	set_element(array_p, &((ts){100, "abc"}), 0);
+
+	print_array(array_p, print_ts);
+
+	set_element(array_p, &((ts){444, "mno"}), 4);
+	set_element(array_p, &((ts){555, "pqr"}), 5);
+
+	print_array(array_p, print_ts);
+
+	set_element(array_p, &((ts){666, "stu"}), 6);
+
+	print_array(array_p, print_ts);
+
+	ts* tsn = (ts*) malloc(sizeof(ts));
+	*tsn = ((ts){10101, "new"});
+	array_p->data_p_p[20] = tsn;
+	print_ts(array_p->data_p_p[20]);
+
+	print_array(array_p, print_ts);
+
+	delete_array(array_p);
+
+	print_array(array_p, print_ts);
 	return 0;
 }
