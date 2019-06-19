375054e618ec25b21a89d6f2fd9a0e96968be860
Rohan Dvivedi <rohandvivedi@gmail.com>
2019-06-19 19:55:52 +0200

    tree interface is atleast ready : der aaye durast aaye, LOL

--- inc/tree.h
+++ inc/tree.h
@@ -67,11 +67,11 @@ tree* get_tree(unsigned long long int children_default_size, unsigned long long 
 // gets you a new node for the specified tree
 node* get_node(const tree* tree_p, const void* data_p);
 
-// adds a child to parent node, as per the description of the characteristics of tree_p
-void add_child(tree* tree_p, node* parent_p, const void* data_p);
+// adds a child to parent node, as per the description of the characteristics of tree_p at index child_index of the parent node of the tree
+void add_child(const tree* tree_p, node* parent_p, const void* data_p, unsigned long long int child_index);
 
-// builds a bond between parent and a child
-void connect(tree* tree_p, node* parent_p, node* child_p);
+// builds a bond between parent node and a child node, placing the child at index child_index of the parent node of the tree
+void build_relation(const tree* tree_p, node* parent_p, node* child_p, unsigned long long int child_index);
 
 // deletes all of the tree
 void delete_tree(tree* tree_p);
@@ -82,10 +82,13 @@ void delete_node(node* node_p);
 // deletes the node and all its children aswell recursively
 void delete_nodes_from(node* node_p);
 
-// prints node
-void print_node(node* node_p);
-
 // print tree
-void print_tree(tree* tree_p, void (*print_data)(const void* node_p));
+void print_tree(const tree* tree_p, void (*print_data)(const void* node_p));
+
+// prints only the node pointed to by node_p
+void print_node(const node* node_p);
+
+// prints node and all of its children aswell recursively
+void print_node(const node* node_p);
 
 #endif
