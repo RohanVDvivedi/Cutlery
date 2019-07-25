3454a20cd7eaffc0ebf2dd7485b804965fd21810
Rohan Dvivedi <rohandvivedi@gmail.com>
2019-07-25 12:14:47 +0200

    only inserts are remaining

--- inc/linkedlist.h
+++ inc/linkedlist.h
@@ -1,4 +1,61 @@
 #ifndef LINKEDLIST_H
 #define LINKEDLIST_H
 
+#include<stdio.h>
+#include<stdlib.h>
+
+// the datastructure of the linkedlist manages its data on its own
+// you manage your own data
+// the pointer you give will be directly stored internally
+// you have to delete your data pointers that you pass
+
+typedef struct node node;
+struct node
+{
+	// the previous node in the linked list
+	node* prev;
+
+	// the data that this node addresses
+	const void* data_p;
+
+	// the next node in the linked list
+	node* next;
+};
+
+typedef struct linkedlist linkedlist;
+struct linkedlist
+{
+	// head->next->...->next = tail
+	// always head->prev = NULL
+	node* head;
+
+	// tail->prev->...->prev = head
+	// always tail->next = NULL
+	node* tail;
+};
+
+// returns a new linked list
+linkedlist* get_linkedlist();
+
+// inserts a new head, the previous head is now next to the new head
+void insert_head(linkedlist* ll,const void* data_p);
+
+// inserts a new tail, the previous tail is now prev to the new tail
+void insert_tail(linkedlist* ll,const void* data_p);
+
+// removes the head, now the new head is next of the previous head
+void remove_head(linkedlist* ll);
+
+// removes the tail, now the new tail is prev of the previous head
+void remove_tail(linkedlist* ll);
+
+// simply gets head data
+const void* get_head_data(linkedlist* ll);
+
+// simply gets tail
+const void* get_tail_data(linkedlist* ll);
+
+// deletes the linked list and all of its nodes
+void delete_linkedlist(linkedlist* ll);
+
 #endif
--- src/linkedlist.c
+++ src/linkedlist.c
@@ -1,1 +1,77 @@
 #include<linkedlist.h>
+
+linkedlist* get_linkedlist()
+{
+	linkedlist* ll = (linkedlist*) calloc(1, sizeof(linkedlist));
+	ll->head = NULL;
+	ll->tail = NULL;
+	return ll;
+}
+
+node* get_new_node(const void* data_p)
+{
+	node* node_p = (node*) calloc(1, sizeof(node));
+	return node_p;
+}
+
+void insert_head(linkedlist* ll, const void* data_p)
+{
+
+}
+
+void insert_tail(linkedlist* ll, const void* data_p)
+{
+
+}
+
+void remove_head(linkedlist* ll)
+{
+	node* old_head = ll->head;
+	if(old_head != NULL)
+	{
+		ll->head = old_head->next;
+		if(ll->head != NULL)
+		{
+			ll->head->prev = NULL;
+		}
+		delete_node(old_head);
+	}
+}
+
+void remove_tail(linkedlist* ll)
+{
+	node* old_tail = ll->tail;
+	if(old_tail != NULL)
+	{
+		ll->tail = old_tail->prev;
+		if(ll->tail != NULL)
+		{
+			ll->tail->next = NULL;
+		}
+		delete_node(old_tail);
+	}
+}
+
+const void* get_head_data(linkedlist* ll)
+{
+	return ll == NULL ? NULL : (ll->head == NULL ? NULL : ll->head->data_p) ;
+}
+
+const void* get_tail_data(linkedlist* ll)
+{
+	return ll == NULL ? NULL : (ll->tail == NULL ? NULL : ll->tail->data_p) ;
+}
+
+void delete_node(node* node_p)
+{
+	free(node_p);
+}
+
+void delete_linkedlist(linkedlist* ll)
+{
+	while(ll->head != NULL)
+	{
+		remove_head(ll);
+	}
+	free(ll);
+}
