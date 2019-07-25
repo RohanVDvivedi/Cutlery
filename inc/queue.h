ba4ded48ac1f03354b6e03ee8084f951696664d7
Rohan Dvivedi <rohandvivedi@gmail.com>
2019-07-25 20:38:56 +0200

    defined stack and queue and declared the methods that they should implement

--- inc/queue.h
+++ inc/queue.h
@@ -1,4 +1,34 @@
 #ifndef QUEUE_H
 #define QUEUE_H
 
+// queue will only hold your pointer as it is,
+// queue does not create/clone/update/mutate your data
+// queue does not shallow or deep clone it
+// you manage(create delete) your own data,
+
+// the below is a array based ring buffer implementation of queue
+
+typedef struct queue queue;
+struct queue
+{
+	array* queueHolder;
+	unsigned long long int firstElementIndex;
+	unsigned long long int lastElementIndex;
+};
+
+// build and get a new queue
+queue* get_queue();
+
+// push a new element to the queue
+void push(queue* queue_p, const void* data_p);
+
+// pop the element from the queue which was inserted earliest of all other elements
+void pop(queue* queue_p);
+
+// returns pointer to the top element data pointer, (i.e. data pointer to the earliest inserted element) 
+const void* get_top(queue* queue_p);
+
+// delete queue and queueHolder array
+void delete_queue(queue* queue_p);
+
 #endif
--- inc/stack.h
+++ inc/stack.h
@@ -1,4 +1,33 @@
 #ifndef STACK_H
 #define STACK_H
 
+// stack will only hold your pointer as it is,
+// stack does not create/clone/update/mutate your data
+// stack does not shallow or deep clone it
+// you manage(create delete) your own data,
+
+// the below is a array based stack implementation of stack
+
+typedef struct stack stack;
+struct stack
+{
+	array* stackHolder;
+	unsigned long long int lastElementIndex;
+};
+
+// build and get a new stack
+stack* get_stack();
+
+// push a new element to the stack
+void push(stack* stack_p, const void* data_p);
+
+// pop the last inserted element from the stack
+void pop(stack* stack_p);
+
+// returns pointer to the top element data pointer, (i.e. data pointer to the last inserted element) 
+const void* get_top(stack* stack_p);
+
+// delete stack and stackHolder array
+void delete_stack(stack* stack_p);
+
 #endif
