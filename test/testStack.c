9257506ab9a46559e59c2d4088aab0c17e7bba57
Rohan Dvivedi <rohandvivedi@gmail.com>
2019-07-26 13:44:42 +0200

    testQueue and testStack have been added

---
+++ test/testStack.c
@@ -0,0 +1,18 @@
+#include<stack.h>
+
+typedef struct teststruct ts;
+struct teststruct
+{
+	int a;
+	char* s;
+};
+
+void print_ts(const void* tsv)
+{
+	printf(" %d, %s", ((ts*)tsv)->a, ((ts*)tsv)->s);
+}
+
+int main()
+{
+	return 0;
+}
