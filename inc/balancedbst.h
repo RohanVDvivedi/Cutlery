d166abaa16f517ea08cdecea45a84f3d5c83a6ec
Rohan Dvivedi <rohandvivedi@gmail.com>
2019-07-29 00:03:21 +0200

    started working on balanced binary search tree

--- inc/balancedbst.h
+++ inc/balancedbst.h
@@ -1,6 +1,16 @@
 #ifndef BALANCEDBST_H
 #define BALANCEDBST_H
 
+#include<stdio.h>
+#include<stdlib.h>
+
+// the tree manages all of its data on its own
+// you manage your own data
+// all the nodes on the left sub tree of the node are smaller than or equal to the node itself,
+// when compare using the data_compare function
+// all the nodes on the right sub tree of the node are greater than the node itself,
+// when compare using the data_compare function
+
 typedef enum tree_type tree_type;
 enum tree_type
 {
@@ -17,19 +27,21 @@ struct node
 	// the data to store
 	const void* data_p;
 
-	// there can be only two child nodes,
-	// we store here pointer to both of them here
-	node* child_nodes[2];
+	// the property of the node that will be used to help balance the tree
+	unsigned long long int node_property;
+
+	// all <= nodes									// all > nodes
+	node* left_sub_tree;							node* right_sub_tree;
 };
 
 typedef struct balancedbst balancedbst;
 struct balancedbst
 {
 	// the type of balancing algorithm to use
-	const tree_type balanced_tree_type;
+	tree_type balanced_tree_type;
 
 	// compare data pointers that we stored
-	// it returns 0 if they are same, 1 if data0 is greater than data1 else it must return -1
+	// it returns 0 if they are same, >0 if data0 is greater than data1 else it must return <0 value
 	int (*data_compare)(const void* data0_p, const void* data1_p);
 
 	// the root node of the tree
@@ -37,7 +49,7 @@ struct balancedbst
 };
 
 // get a new balancedbst
-balancedbst* get_balancedbst();
+balancedbst* get_balancedbst(tree_type balanced_tree_type, int (*data_compare)(const void* data0_p, const void* data1_p));
 
 // inserts a node in the tree whose data is data_p
 void insert(balancedbst* blanacedbst_p, const void* data_p);
@@ -46,7 +58,7 @@ void insert(balancedbst* blanacedbst_p, const void* data_p);
 const void* find(balancedbst* blanacedbst_p, const void* data_p);
 
 // remove the node if found, returns a list of nodes that match
-void remove(balancedbst* blanacedbst_p, const void* data_p);
+void remove_node(balancedbst* blanacedbst_p, const void* data_p);
 
 // delete the balancedbst and all its nodes
 void delete_balancedbst(balancedbst* balancedbst_p);
--- src/balancedbst.c
+++ src/balancedbst.c
@@ -3,24 +3,96 @@
 balancedbst* get_balancedbst(tree_type balanced_tree_type, int (*data_compare)(const void* data0_p, const void* data1_p))
 {
 	balancedbst* balancedbst_p = (balancedbst*) calloc(1, sizeof(balancedbst));
-	balancedbst->balanced_tree_type = balanced_tree_type;
+	balancedbst_p->balanced_tree_type = balanced_tree_type;
 	balancedbst_p->root = NULL;
 	balancedbst_p->data_compare = data_compare;
+	return balancedbst_p;
+}
+
+void insert_in_red_black_tree(balancedbst* blanacedbst_p, const void* data_p)
+{
+
+}
+
+void insert_in_avl_tree(balancedbst* blanacedbst_p, const void* data_p)
+{
+
 }
 
 void insert(balancedbst* blanacedbst_p, const void* data_p)
 {
+	switch(blanacedbst_p->balanced_tree_type)
+	{
+		case RED_BLACK_TREE :
+		{
+			insert_in_red_black_tree(blanacedbst_p, data_p);
+			break;
+		}
+		case AVL_TREE :
+		{
+			insert_in_avl_tree(blanacedbst_p, data_p);
+			break;
+		}
+	}
+}
 
+const void* find_node(balancedbst* blanacedbst_p, const node* root, const void* data_p)
+{
+	if(blanacedbst_p->data_compare(data_p, root->data_p) == 0)
+	{
+		return root->data_p;
+	}
+	else if(root->left_sub_tree != NULL && blanacedbst_p->data_compare(data_p, root->data_p) < 0)
+	{
+		return find_node(blanacedbst_p, root->left_sub_tree, data_p);
+	}
+	else if(root->right_sub_tree != NULL && blanacedbst_p->data_compare(data_p, root->data_p) > 0)
+	{
+		return find_node(blanacedbst_p, root->right_sub_tree, data_p);
+	}
+	else
+	{
+		return NULL;
+	}
 }
 
 const void* find(balancedbst* blanacedbst_p, const void* data_p)
 {
+	if(blanacedbst_p->root == NULL)
+	{
+		return NULL;
+	}
+	else
+	{
+		return find_node(blanacedbst_p, blanacedbst_p->root, data_p);
+	}
+}
+
+void remove_from_red_black_tree(balancedbst* blanacedbst_p, const void* data_p)
+{
 
 }
 
-void remove(balancedbst* blanacedbst_p, const void* data_p)
+void remove_from_avl_tree(balancedbst* blanacedbst_p, const void* data_p)
 {
 
+}
+
+void remove_node(balancedbst* blanacedbst_p, const void* data_p)
+{
+	switch(blanacedbst_p->balanced_tree_type)
+	{
+		case RED_BLACK_TREE :
+		{
+			remove_from_red_black_tree(blanacedbst_p, data_p);
+			break;
+		}
+		case AVL_TREE :
+		{
+			remove_from_avl_tree(blanacedbst_p, data_p);
+			break;
+		}
+	}
 }
 
 void delete_node(node* node_p)
@@ -30,18 +102,19 @@ void delete_node(node* node_p)
 
 void delete_nodes_from(node* node_p)
 {
-	if(node_p->child_nodes[0] != NULL)
+	if(node_p->left_sub_tree != NULL)
 	{
-		delete_nodes_from(node_p->child_nodes[0]);
+		delete_nodes_from(node_p->left_sub_tree);
 	}
-	if(node_p->child_nodes[1] != NULL)
+	if(node_p->right_sub_tree != NULL)
 	{
-		delete_nodes_from(node_p->child_nodes[1]);
+		delete_nodes_from(node_p->right_sub_tree);
 	}
 	delete_node(node_p);
 }
 
 void delete_balancedbst(balancedbst* balancedbst_p)
 {
-
+	delete_nodes_from(balancedbst_p->root);
+	free(balancedbst_p);
 }
