3f74b36ccd36f0f1ee1359833d7ce21cf82cbb66
Rohan Dvivedi <rohandvivedi@gmail.com>
2019-09-15 16:22:59 +0530

    did minor refactoring, and increase user readability of the linked list code

--- inc/linkedlist.h
+++ inc/linkedlist.h
@@ -37,11 +37,38 @@ struct linkedlist
 // returns a new linked list
 linkedlist* get_linkedlist();
 
+/*
+	BELOW LINKEDLIST FUNCTIONS CAN BE USED TO IMPLEMENT QUEUES, STACKS AND DOUBLE ENDED QUEUES
+*/
+
+// simply gets head data
+const void* get_head_data(linkedlist* ll);
+
+// simply gets tail
+const void* get_tail_data(linkedlist* ll);
+
 // inserts a new head, the previous head is now next to the new head
 void insert_head(linkedlist* ll, const void* data_p);
 
 // inserts a new tail, the previous tail is now prev to the new tail
 void insert_tail(linkedlist* ll, const void* data_p);
+
+// removes the head, now the new head is next of the previous head
+void remove_head(linkedlist* ll);
+
+// removes the tail, now the new tail is prev of the previous head
+void remove_tail(linkedlist* ll);
+
+/*
+	ABOVE LINKEDLIST FUNCTIONS CAN BE USED TO IMPLEMENT QUEUES, STACKS AND DOUBLE ENDED QUEUES
+*/
+
+
+
+/*
+	BELOW LINKEDLIST FUNCTIONS CAN BE USED TO DO NODE LEVEL MANIPULATIONS
+	TRY NOT TO USE THEM
+*/
 
 // create a node and insert it in linkedlist before node_p
 // here node_p may not be null
@@ -51,20 +78,8 @@ void insert_node_before(linkedlist* ll, node* node_p, const void* data_p);
 // here node_p may not be null
 void insert_node_after(linkedlist* ll, node* node_p, const void* data_p);
 
-// removes the head, now the new head is next of the previous head
-void remove_head(linkedlist* ll);
-
-// removes the tail, now the new tail is prev of the previous head
-void remove_tail(linkedlist* ll);
-
 // remove the given node from the linked list
 void remove_node(linkedlist*ll, node* node_p);
-
-// simply gets head data
-const void* get_head_data(linkedlist* ll);
-
-// simply gets tail
-const void* get_tail_data(linkedlist* ll);
 
 // get pointer to nth element from head by doing next next
 const node* get_nth_node_from_head(linkedlist* ll, unsigned long long int n);
@@ -72,13 +87,19 @@ const node* get_nth_node_from_head(linkedlist* ll, unsigned long long int n);
 // get pointer to nth element from tail by doing next next
 const node* get_nth_node_from_tail(linkedlist* ll, unsigned long long int n);
 
+/*
+	ABOVE LINKEDLIST FUNCTIONS CAN BE USED TO DO NODE LEVEL MANIPULATIONS
+	TRY NOT TO USE THEM
+*/
+
+
 // deletes the linked list and all of its nodes
 void delete_linkedlist(linkedlist* ll);
 
 // perform operation on all the elements of the linked list
 void for_each_in_list(const linkedlist* ll, void (*operation)(void* data_p, const void* additional_params), const void* additional_params);
 
-// returns pointer to node of the element found in ll, if compare between the element and data_p returns 1
+// returns pointer to data_p of the element found in ll, if compare between the element and data_p returns 1
 const void* find_first_in_list(const linkedlist* ll, void* data_p, int (*compare)(const void* data_p1, const void* data_p2, const void* additional_params), const void* additional_params);
 
 // returns 1 if a node with data_p was found and was deleted, else returns 0
