145986b2a7626ffd7b2849397920ffcf155059ef
Rohan Dvivedi <rohandvivedi@gmail.com>
2019-10-11 19:16:20 +0530

    added compilable code, and the now normalized code base will help us gain getter control on the calls

--- inc/datastructure.h
+++ inc/datastructure.h
@@ -13,22 +13,13 @@ enum datastructuretype
 	HEAP
 };
 
-// this is bucketted datastructures specific enum
-// for the put_entry operation
-typedef enum put_type put_type;
-enum put_type
-{
-	PUT_IF_EXISTS 		= 0x01,	// update = 01 decimal
-	PUT_IF_NOT_EXISTS 	= 0x10	// insert = 16 decimal
-};
-
 typedef struct datastructure datastructure;
 struct datastructure
 {
 	dstype type;
 };
 
-ds* get_datastructure(dstype type, int sub_type, unsigned long long int size, int (*key_compare)(const void* key1, const void* key2), unsigned long long int (*hash_function)(const void* key));
+datastructure* get_datastructure(dstype type, int sub_type, unsigned long long int size, int (*key_compare)(const void* key1, const void* key2), unsigned long long int (*hash_function)(const void* key));
 
 // returns true for hashmap, bst, linkedlist if it is bucketted and heap
 int is_bucketted(datastructure* ds_p);
@@ -37,25 +28,31 @@ int is_bucketted(datastructure* ds_p);
 int is_unique_keyed(datastructure* ds_p);
 
 // returns true for stack, queu and heap
-int is_push_pop_type(datastructure* ds_p)
+int is_push_pop_type(datastructure* ds_p);
 
 // put a new entry
-void put_entry(datastructure* ds_p, const void* key, const void* value, put_type ptype);
+void insert_entry(datastructure* ds_p, const void* key, const void* value);
 
 // get a new value
 const void* find_value(const datastructure* ds_p, const void* input);
 
+// get a new value
+int update_value(const datastructure* ds_p, const void* input, const void** return_value);
+
 // returns 1 if the value is found and removed
-int remove_value(datastructure* ds_p, const void* key, const void** return_key, const void** return_value);
+int delete_entry(datastructure* ds_p, const void* key, const void** return_key, const void** return_value);
 
 // push a new element
 void push(datastructure* ds_p, const void* data_p);
+
+// push a new entry
+void push_entry(datastructure* ds_p, const void* key_p, const void* value_p);
 
 // pop the element
 void pop(datastructure* ds_p);
 
 // returns pointer to the top element data pointer, (i.e. data pointer to the element will be next popped
-const void* get_top(stack* stack_p);
+const void* get_top(datastructure* ds_p);
 
 void delete_datastructure(datastructure* ds_p);
 
--- src/datastructure.c
+++ src/datastructure.c
@@ -1,1 +1,66 @@
 #include<datastructure.h>
+
+datastructure* get_datastructure(dstype type, int sub_type, unsigned long long int size, int (*key_compare)(const void* key1, const void* key2), unsigned long long int (*hash_function)(const void* key))
+{
+
+}
+
+int is_bucketted(datastructure* ds_p)
+{
+
+}
+
+int is_unique_keyed(datastructure* ds_p)
+{
+
+}
+
+int is_push_pop_type(datastructure* ds_p)
+{
+
+}
+
+void insert_entry(datastructure* ds_p, const void* key, const void* value)
+{
+
+}
+
+const void* find_value(const datastructure* ds_p, const void* input)
+{
+
+}
+
+int update_value(const datastructure* ds_p, const void* input, const void** return_value)
+{
+
+}
+
+int delete_entry(datastructure* ds_p, const void* key, const void** return_key, const void** return_value)
+{
+
+}
+
+void push(datastructure* ds_p, const void* data_p)
+{
+
+}
+
+void push_entry(datastructure* ds_p, const void* key_p, const void* value_p)
+{
+
+}
+
+void pop(datastructure* ds_p)
+{
+
+}
+
+const void* get_top(datastructure* ds_p)
+{
+
+}
+
+void delete_datastructure(datastructure* ds_p)
+{
+
+}
