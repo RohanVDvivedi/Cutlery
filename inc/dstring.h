19c64b81e5d851525cba1ce48d594a5e9bc57066
Rohan Dvivedi <rohandvivedi@gmail.com>
2019-10-12 02:17:21 +0530

    added styring type that will help us a lot

---
+++ inc/dstring.h
@@ -0,0 +1,30 @@
+#ifndef DSTRING_H
+#define DSTRING_H
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+typedef struct dstring dstring;
+struct dstring
+{
+	char* cstring;
+	unsigned long long int bytes_occupied;
+	unsigned long long int bytes_allocated;
+};
+
+dstring* get_dstring(const char* cstr_p, unsigned long long int additional_allocation);
+
+// returns 1, if str_p1 > str_p2, returns 0 if they are equal else returns -1
+int compare(const dstring* str_p1, const dstring* str_p2);
+
+// takes a dstring as parameter, and returns a new dstring, that is of approximately double the size 
+dstring* expand(dstring* str_p, unsigned long long int additional_allocation);
+
+dstring* append(dstring* str_p, char* cstr_p);
+
+dstring* concatenate(dstring* str_p1, dstring* str_p2);
+
+void delete_dstring(dstring* str_p);
+
+#endif
---
+++ src/dstring.c
@@ -0,0 +1,54 @@
+#include<dstring.h>
+
+dstring* get_dstring(const char* cstr_p, unsigned long long int additional_allocation)
+{
+	dstring* str_p = (dstring*)malloc(sizeof(dstring));
+	str_p->bytes_occupied = cstr_p == NULL ? 1 : (strlen(cstr_p) + 1);
+	str_p->bytes_allocated = (2 * (str_p->bytes_occupied + 1)) + 2 + additional_allocation;
+	str_p->cstring = (char*)malloc(str_p->bytes_allocated);
+	str_p->cstring[0] = '\0';
+	if(cstr_p != NULL)
+	{
+		memcpy(str_p->cstring, cstr_p, str_p->bytes_occupied);
+	}
+	return str_p;
+}
+
+int compare(const dstring* str_p1, const dstring* str_p2)
+{
+	return strcmp(str_p1->cstring, str_p2->cstring);
+}
+
+dstring* expand(dstring* str_p, unsigned long long int additional_allocation)
+{
+	dstring* expanded_dstring = get_dstring(str_p->cstring, additional_allocation);
+	delete_dstring(str_p);
+	return expanded_dstring;
+}
+
+dstring* append(dstring* str_p, char* cstr_p)
+{
+	int occ = cstr_p == NULL ? 1 : strlen(cstr_p);
+	if( occ + str_p->bytes_occupied - 1 > str_p->bytes_allocated)
+	{
+		str_p = expand(str_p, (2 * occ) + 2);
+	}
+	if(cstr_p != NULL)
+	{
+		memcpy(str_p->cstring + str_p->bytes_occupied - 1, cstr_p, occ);
+	}
+	return str_p;
+}
+
+dstring* concatenate(dstring* str_p1, dstring* str_p2)
+{
+	str_p1 = append(str_p1, str_p2->cstring);
+	delete_dstring(str_p2);
+	return str_p1;
+}
+
+void delete_dstring(dstring* str_p)
+{
+	free(str_p->cstring);
+	free(str_p);
+}
