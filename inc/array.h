774489be77d4cf64b10641600556b0da383cc170
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-05-07 11:04:51 +0530

    change in comments

--- inc/array.h
+++ inc/array.h
@@ -42,10 +42,7 @@ void delete_array(array* array_p);
 // returns pointer to the data at index = index
 const void* get_element(const array* array_p, unsigned long long int index);
 
-// if index element is NULL, allocates new memory sets new element there 
-// and deletes old data permanently
-// if not null, old data memory is rewritten, with new data
-// returns 0 for success
+// returns 0 for success, -1 for index out of bounds
 int set_element(array* array_p, const void* data_p, unsigned long long int index);
 
 // perform operation on all the elements of the array, the method operation takes in 2 params, the data_p to operation on and its index
