68fe18900e5489c868429842f0ced455a8e8b008
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-06-08 20:40:39 +0530

    using dstring on stack for tests

--- test/testDstring.c
+++ test/testDstring.c
@@ -4,36 +4,38 @@
 
 int main()
 {
-	dstring* str = get_dstring("", 1);
-	display_dstring(str);printf("\n");
+	dstring str;
+	init_dstring(&str, "", 1);
+	display_dstring(&str);printf("\n");
 
-	append_to_dstring(str, "Hello ");
-	display_dstring(str);printf("\n");
+	append_to_dstring(&str, "Hello ");
+	display_dstring(&str);printf("\n");
 
-	dstring* tempstr = get_dstring("World, as said by Rohan\n", 2);
-	concatenate_dstring(str, tempstr);
-	delete_dstring(tempstr);
-	display_dstring(str);printf("\n");
+	dstring tempstr;
+	init_dstring(&tempstr, "World, as said by Rohan\n", 2);
+	concatenate_dstring(&str, &tempstr);
+	deinit_dstring(&tempstr);
+	display_dstring(&str);printf("\n");
 
-	display_dstring(str);printf("\n");
+	display_dstring(&str);printf("\n");
 	char* temp = NULL;
 
 	temp = "";
-	printf("prefix %s %d\n", temp, is_prefix(str, temp));
+	printf("prefix %s %d\n", temp, is_prefix(&str, temp));
 	temp = "H";
-	printf("prefix %s %d\n", temp, is_prefix(str, temp));
+	printf("prefix %s %d\n", temp, is_prefix(&str, temp));
 	temp = "Helt";
-	printf("prefix %s %d\n", temp, is_prefix(str, temp));
+	printf("prefix %s %d\n", temp, is_prefix(&str, temp));
 	temp = "Hell";
-	printf("prefix %s %d\n", temp, is_prefix(str, temp));
+	printf("prefix %s %d\n", temp, is_prefix(&str, temp));
 	temp = "Hello World";
-	printf("prefix %s %d\n", temp, is_prefix(str, temp));
+	printf("prefix %s %d\n", temp, is_prefix(&str, temp));
 	temp = "Hello Worlds";
-	printf("prefix %s %d\n", temp, is_prefix(str, temp));
+	printf("prefix %s %d\n", temp, is_prefix(&str, temp));
 	temp = "Hello World, as said by Rohan\n";
-	printf("prefix %s %d\n", temp, is_prefix(str, temp));
+	printf("prefix %s %d\n", temp, is_prefix(&str, temp));
 	temp = "Hello World, as said by Rohan\n, Hello World";
-	printf("prefix %s %d\n", temp, is_prefix(str, temp));
+	printf("prefix %s %d\n", temp, is_prefix(&str, temp));
 
-	delete_dstring(str);
+	deinit_dstring(&str);
 }
