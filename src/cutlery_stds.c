bb90235547e4f9bfafccaa5365965bde11cbd246
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-11-02 11:11:53 +0530

    implemented a very basic memory move function

--- src/cutlery_stds.c
+++ src/cutlery_stds.c
@@ -1,6 +1,29 @@
 #include<cutlery_stds.h>
 
-void memory_move(void* dest, const void* src, unsigned int size)
+void memory_move(void* dest_start, const void* src_start, unsigned int size)
 {
-	
+	// if they are the same memory locations, skip the copy operation
+	if(src == dest)
+		return;
+
+	// calcualte the last src and dest addresses
+	void* dest_end = dest_start + size - 1;
+	const void* src_end = src_start + size - 1;
+
+	if(dest_start < src_start) // make forward pass
+	{
+		void* dest = dest_start;
+		const void* src = src_start;
+
+		while(src <= src_end)
+			*(dest++) = *(src++);
+	}
+	else // else make backward pass
+	{
+		void* dest = dest_end;
+		const void* src = src_end;
+
+		while(src >= src_start)
+			*(dest--) = *(src--);
+	}
 }
