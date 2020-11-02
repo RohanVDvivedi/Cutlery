d34c637458be1a5a8f0486991891e567a4ffb92a
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-11-02 12:35:46 +0530

    removed redundant statements from memory_move

--- src/cutlery_stds.c
+++ src/cutlery_stds.c
@@ -6,22 +6,18 @@ void memory_move(void* dest_start, const void* src_start, unsigned int size)
 	if(src_start == dest_start)
 		return;
 
-	// calcualte the last src and dest addresses
-	void* dest_end = dest_start + size - 1;
-	const void* src_end = src_start + size - 1;
-
 	if(dest_start < src_start) // make forward pass
 	{
 		char* dest = dest_start;
 		const char* src = src_start;
 
-		while(src <= ((char*)(src_end)))
+		while(src <= ((char*)(src_start + size - 1)))
 			*(dest++) = *(src++);
 	}
 	else // else make backward pass
 	{
-		char* dest = dest_end;
-		const char* src = src_end;
+		char* dest = dest_start + size - 1;
+		const char* src = src_start + size - 1;
 
 		while(src >= ((char*)(src_start)))
 			*(dest--) = *(src--);
