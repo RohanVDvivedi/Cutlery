70e6e979b68d73fda2caf8c33eade6908c6fe995
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-11-02 10:26:37 +0530

    added notes for implementation of memory move

--- inc/cutlery_stds.h
+++ inc/cutlery_stds.h
@@ -3,6 +3,15 @@
 
 #define NULL ((void*)0)
 
+/* this is cutlery specific memory move implementation
+   this is the function to be used in place of memcpy/memmove std library functions
+   features : 	
+		* moves size number of bytes from src to dest pointer
+		* There is protection againat the memory locations overlap (similar to memmove)
+		  * this is solved by reverse iteration for copying memory
+		* it copies processor accessible word side if and when possible
+		  * this function copies unsigned int instead of unsigned char where ever possible
+*/
 void memory_move(void* dest, const void* src, unsigned int size);
 
 #endif
