f01ed48e450818269dfccbd48d987d1c0a22f746
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-11-03 18:49:05 +0530

    prefix and suffix unaligned bytes are copied byte by byte and rest are copied int by int in the forward direction, if atleast 2 int copy iterations are required

--- src/cutlery_stds.c
+++ src/cutlery_stds.c
@@ -6,42 +6,62 @@ void memory_move(void* dest_start, const void* src_start, unsigned int size)
 	if(src_start == dest_start || size == 0)
 		return;
 
-	// the last byte address that needs to be copied
-	const void* src_end = src_start + size - 1;
+	// the end of the src buffer and last src byte address that needs to be copied
+	const void* src_end = src_start + size;
+	const void* src_last = src_end - 1;
 
 	// make forward pass
 	// if there is no overlap or if the overlap doesnt corrupt the copy operation
 	if(dest_start < src_start || src_end < dest_start)
 	{
-		unsigned int int_size = sizeof(int);
-		unsigned int int_alignment_bit_mask = -((int)int_size);
+		char* dest = dest_start;
+		const char* src = src_start;
 
-		// check if int copy is possible
-		if( (size >= 16) && !(size & ~int_alignment_bit_mask)
-			&& !(((unsigned long int)src_start) & ~int_alignment_bit_mask)
-			&& !(((unsigned long int)dest_start) & ~int_alignment_bit_mask) )
+		static const unsigned int int_size = sizeof(int); // we assume that int is always 2^n bytes in size, i.e. 1,2,4,8 etc
+		static const unsigned int int_alignment_bit_mask = -((int)int_size);	// 0b 111...111 000...000 where 0s represent alignment
+
+		// check if int copy is possible, and requires atleast 3 iterations (atleast 2 int copy iterations)
+		if( (size >= 3 * int_size) &&
+			(
+				(((long int)src_start) & ~int_alignment_bit_mask) == (((long int)dest_start) & ~int_alignment_bit_mask)
+			)
+		)
 		{
-			int* dest = dest_start;
-			const int* src = src_start;
-
-			while(src < ((int*)(src_start + size)))
+			// perform a byte by byte copy until the address is int aligned
+			while( ((long int)src) & ~int_alignment_bit_mask )
 				*(dest++) = *(src++);
-		}
-		// else just do an old fashioned byte copy
-		else
-		{
-			char* dest = dest_start;
-			const char* src = src_start;
 
-			while(src < ((char*)(src_start + size)))
-				*(dest++) = *(src++);
+			// perform an int transfer in this scope
+			// dest_int and src_int must not leave this scope
+			// they must equal the dest and src, right before and right after this scope
+			{
+				int* dest_int = (int*)dest;
+				const int* src_int = (int*)src;
+
+				// additonal bytes that you might have to copy after completing the int copy
+				unsigned long int additional_bytes = ((unsigned long int)src_end) & ~int_alignment_bit_mask;
+
+				// if you hit this address, stop copying ints
+				const int* src_end_int = src_end - additional_bytes;
+
+				while( src_int < src_end_int )
+					*(dest_int++) = *(src_int++);
+
+				dest = (char*)dest_int;
+				src = (char*)src_int;
+			}
 		}
+
+		// finish up remaining with an old fashioned byte copy
+		while(src < ((char*)(src_end)))
+			*(dest++) = *(src++);
 	}
 	// else make backward pass
 	else
 	{
+		// start from the last byte that needs to be copied
 		char* dest = dest_start + size - 1;
-		const char* src = src_start + size - 1;
+		const char* src = src_last;
 
 		while(src >= ((char*)(src_start)))
 			*(dest--) = *(src--);
