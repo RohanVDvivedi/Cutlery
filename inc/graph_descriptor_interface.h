d3a0421f0c2daf3ece2e4c0fd52cf89e28e5c8ed
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-11-03 11:37:47 +0530

    added quidelines for anyone who would be using the graph_descriptor_interface in the future

--- inc/graph_descriptor_interface.h
+++ inc/graph_descriptor_interface.h
@@ -5,10 +5,31 @@
 // that defines/describes implementation of a graph data structure
 // and access methods to access it by the graph algorithms of cutlery
 
-// here the context defines 
-//  * the query that we are solving
-//  * any current query specific variables
-//  * or a representation of a query that the current graph traversal is solving
+/* 
+	here the context defines and the context should help you know the following
+	  * the query that we are solving (a description of the problem that you are solving)
+	  * the graph that we are solving it on (ideally a pointer to the graph itself)
+	  * any current query specific variables
+	      * this is where you should ideally maintain 
+	        * what nodes have been visited
+	        * what edges have been traversed, etc
+
+	the above points should be used only as a guide line to define what a context datastructure should contain
+	you may aswell feel free to do what ever you like
+
+	BUT here are my 2 cents for your design
+
+	variables in the context should help you evaluate the all the following descriptor functions quickly
+	storing the query description will help you filter what edges are not needed to be traversed for the problem at hand
+	effectively sppeding up the graph traversal
+
+	the context has to be discardable after the query or search algorithm or path finding algorithm
+	so that you dont need to ""unvisit"" the visisted nodes and ""untraverse"" the traversed edges
+
+	keeping the context specific variables will also help you segregate actual data vs temporary data of the graph (like visited node and edge specific data)
+	having the traversal context separate from the graph will help you to parallelize algorithms
+	by supplying separate new context to run read-only graph traversals all in different threads concurrently
+*/
 
 typedef struct node_descriptor node_descriptor;
 struct node_descriptor
@@ -20,7 +41,7 @@ struct node_descriptor
 	// get an edge that is touching this node i.e. this node is either source or destination for a given edge
 	// here the edge_index is always lesser than the result of get_outgoing_edge_count
 	// it varies from 0 to get_outgoing_edge_count() - 1
-	void* (*get_edge)(void* node, void* context, unsigned int edge_index);
+	void* (*get_edge)(void* node, unsigned int edge_index, void* context);
 
 	// to mark a given node as visited and
 	// to check if a given node has been visited ( 1 if visited, 0 if not visited )
@@ -43,8 +64,8 @@ struct edge_descriptor
 	// for any of DIRECTED or UNDIRECTED edge, the node_index can be 0 or 1 only. (node_index < 2 always)
 	// in a DIRECTED edge, node_index may be equal to FROM_NODE or TO_NODE
 	//    node_index == FROM_NODE  => return source node (edge comming out)
-	//    node_index == TO_node    => return destination node (edge going in)
-	void* (*get_node)(void* edge, void* context, unsigned int node_index);
+	//    node_index == TO_NODE    => return destination node (edge going in)
+	void* (*get_node)(void* edge, unsigned int node_index, void* context);
 
 	// this is the cost incurred while traversing this particular edge
 	int (*get_traversal_cost)(void* edge, void* context);
