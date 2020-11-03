ba9ff3f9e3a45e97760098e4bc88008996796f91
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-11-03 10:36:22 +0530

    added very basic integer based copy, that works only if the src and destination addresses are int aligned, and size to be copied is multiple of size of int

--- src/cutlery_stds.c
+++ src/cutlery_stds.c
@@ -13,11 +13,29 @@ void memory_move(void* dest_start, const void* src_start, unsigned int size)
 	// if there is no overlap or if the overlap doesnt corrupt the copy operation
 	if(dest_start < src_start || src_end < dest_start)
 	{
-		char* dest = dest_start;
-		const char* src = src_start;
+		unsigned int int_size = sizeof(int);
+		unsigned int int_alignment_bit_mask = -((int)int_size);
 
-		while(src < ((char*)(src_start + size)))
-			*(dest++) = *(src++);
+		// check if int copy is possible
+		if( (size >= 16) && !(size & ~int_alignment_bit_mask)
+			&& !(((unsigned long int)src_start) & ~int_alignment_bit_mask)
+			&& !(((unsigned long int)dest_start) & ~int_alignment_bit_mask) )
+		{
+			int* dest = dest_start;
+			const int* src = src_start;
+
+			while(src < ((int*)(src_start + size)))
+				*(dest++) = *(src++);
+		}
+		// else just do an old fashioned byte copy
+		else
+		{
+			char* dest = dest_start;
+			const char* src = src_start;
+
+			while(src < ((char*)(src_start + size)))
+				*(dest++) = *(src++);
+		}
 	}
 	// else make backward pass
 	else
