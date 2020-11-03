21ca007f75036b316c3b2b417d89a87acc49d540
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-11-03 10:09:40 +0530

    copy only if there are non zero bytes to be copied, isolate reverse copy case only if copy could corrupt memory

--- src/cutlery_stds.c
+++ src/cutlery_stds.c
@@ -3,10 +3,15 @@
 void memory_move(void* dest_start, const void* src_start, unsigned int size)
 {
 	// if they are the same memory locations, skip the copy operation
-	if(src_start == dest_start)
+	if(src_start == dest_start || size == 0)
 		return;
 
-	if(dest_start < src_start) // make forward pass
+	// the last byte address that needs to be copied
+	const void* src_end = src_start + size - 1;
+
+	// make forward pass
+	// if there is no overlap or if the overlap doesnt corrupt the copy operation
+	if(dest_start < src_start || src_end < dest_start)
 	{
 		char* dest = dest_start;
 		const char* src = src_start;
@@ -14,7 +19,8 @@ void memory_move(void* dest_start, const void* src_start, unsigned int size)
 		while(src < ((char*)(src_start + size)))
 			*(dest++) = *(src++);
 	}
-	else // else make backward pass
+	// else make backward pass
+	else
 	{
 		char* dest = dest_start + size - 1;
 		const char* src = src_start + size - 1;
