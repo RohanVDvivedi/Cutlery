5f7abf0527fa41b36993bd131f2c789f872e65f1
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-11-05 18:40:58 +0530

    modified memory_allocator_interface to accomodate a single call to avail allocation facility of all of malloc, calloc, realloc and aligned_alloc

--- inc/memory_allocator_interface.h
+++ inc/memory_allocator_interface.h
@@ -2,24 +2,47 @@
 #define MEMORY_ALLOCATOR_INTERFACE_H
 
 // A memory allocator is a struct thet contains methods for allocation/deallocation of heap memory
-// it defines a memory allocator methods to be used by a container data structure
+// it defines a memory allocator methods to be used by a container data structure of this library (Cutlery)
 // you may provide a custom memory allocator for any specific datastructure container for cutlery
-// the memory allocator functions definitions are kept so as to be equivalent to heap memory allocator of stdlib.h 
 
 typedef struct memory_allocator memory_allocator;
 struct memory_allocator
 {
-	// usage similar to malloc
-	void* (*allocate)(unsigned int size);
-
-	// usage similar to calloc
-	void* (*allocate_zero)(unsigned int number_of_elements, unsigned int size_of_each_element);
-
-	// usage similar to realloc
-	void* (*reallocate)(void* old_allocation, unsigned int size);
+	// usage of this function is sohisticated and complex
+	// it has been designed to encapsualte the functionality of all of
+	// 		* malloc, calloc, realloc and aligned_alloc (or posix_memalign)
+	/*
+	** parameters
+	**
+	** old_memory                 : memory allocated in the past, that you no longer require, this memory must/will be freed
+	** old_size                   : size of memory pointed to by old_allocation (your custom may or may not use this value, your call)
+	** new_size                   : size of new memory that you want
+	** new_alignment              : the alignment reuired for the new memory, this is of size new_size
+	** zero_initialize_new_memory : if this parameter is non zero, then the new memory must be nonzero
+	**
+	**
+	** returns
+	** 		if the new_size != 0 then,
+	**			this a pointer to a new memory address,
+	** 			of size equal to new_size,
+	**			with its first address aligned to new_alignment,
+	**			all the returned memory must be 0, if (zero_initialize_new_memory != 0)
+	**		else
+	**			this function returns NULL ( ((void*)0) as defined in cutlery_stds.h )
+	**
+	**	this function must return NULL on any error that you encounter,
+	**	inclusive but not limited to
+	**		* memory allocation failure due to excessive memory utilization
+	*/
+	void* (*allocate)(void* old_memory, unsigned int old_size, unsigned int new_size, unsigned int new_alignment, int zero_initialize_new_memory);
 
 	// usage similar to free
-	void (*deallocate)(void* old_allocation);
+	/*
+	** 	this is equivalent to calling
+	** 	**	allocate(old_memory, old_size, 0, 0, 0) **
+	**	and returning NULL
+	*/
+	void (*deallocate)(void* old_memory, unsigned int old_size);
 };
 
 #endif
