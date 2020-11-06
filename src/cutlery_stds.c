517200874585d01ada41644fb3696c093e41fa93
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-11-06 16:08:55 +0530

    correctness of memory_move implementation verified by testcases

--- src/cutlery_stds.c
+++ src/cutlery_stds.c
@@ -12,7 +12,7 @@ void memory_move(void* dest_start, const void* src_start, unsigned int size)
 
 	// make forward pass
 	// if there is no overlap or if the overlap doesnt corrupt the copy operation
-	if(dest_start < src_start || src_end < dest_start)
+	if(dest_start < src_start  || src_last < dest_start)
 	{
 		char* dest = dest_start;
 		const char* src = src_start;
