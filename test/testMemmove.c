e9c70f65d60c7f0a4aff77c7db722af2d7aede8b
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-11-06 15:41:33 +0530

    added test case for memory_move function

---
+++ test/testMemmove.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#include<cutlery_stds.h>
+
+void init_rand_memory(char* mem, int size)
+{
+	while(size-- > 0)
+		*(mem++) = (rand() % 26) + 'A';
+}
+
+void print_memory(char* mem, int size)
+{
+	printf("@ %p %d\n", mem, size);
+	while(size-- > 0)
+		printf("%c ", (*(mem++)));
+	printf("\n");
+}
+
+int main()
+{
+	const int test_iter = 8;
+	const int test_memory_size = 20;
+
+	char test_memory_src[test_memory_size + test_iter];
+	char test_memory_dest[test_memory_size + test_iter];
+
+	for(int i = 0; i < test_iter; i++)
+	{
+		for(int j = 0; j < test_iter; j++)
+		{
+			init_rand_memory(test_memory_src + i, test_memory_size);
+			init_rand_memory(test_memory_dest + j, test_memory_size);
+
+			printf("\nInitial : \n");
+			printf("src:");
+			print_memory(test_memory_src + i, test_memory_size);
+			printf("dest:");
+			print_memory(test_memory_dest + j, test_memory_size);
+
+			memory_move(test_memory_dest + j, test_memory_src + i, test_memory_size);
+
+
+			printf("Result : \n");
+			printf("dest:");
+			print_memory(test_memory_dest + j, test_memory_size);
+		}
+	}
+
+	return 0;
+}
