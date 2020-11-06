48e9b9fd028c7dbb1f4192bf8d16704faac91b7e
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-11-06 16:08:55 +0530

    correctness of memory_move implementation verified by testcases

--- test/testMemmove.c
+++ test/testMemmove.c
@@ -20,30 +20,51 @@ void print_memory(char* mem, int size)
 int main()
 {
 	const int test_iter = 8;
-	const int test_memory_size = 20;
+	const int test_memory_size = 22;
 
+	char test_memory_dest_backward[test_memory_size + test_iter];
 	char test_memory_src[test_memory_size + test_iter];
-	char test_memory_dest[test_memory_size + test_iter];
+	char test_memory_dest_forward[test_memory_size + test_iter];
 
 	for(int i = 0; i < test_iter; i++)
 	{
 		for(int j = 0; j < test_iter; j++)
 		{
 			init_rand_memory(test_memory_src + i, test_memory_size);
-			init_rand_memory(test_memory_dest + j, test_memory_size);
+			init_rand_memory(test_memory_dest_forward + j, test_memory_size);
 
 			printf("\nInitial : \n");
 			printf("src:");
 			print_memory(test_memory_src + i, test_memory_size);
 			printf("dest:");
-			print_memory(test_memory_dest + j, test_memory_size);
+			print_memory(test_memory_dest_forward + j, test_memory_size);
 
-			memory_move(test_memory_dest + j, test_memory_src + i, test_memory_size);
-
+			memory_move(test_memory_dest_forward + j, test_memory_src + i, test_memory_size);
 
 			printf("Result : \n");
 			printf("dest:");
-			print_memory(test_memory_dest + j, test_memory_size);
+			print_memory(test_memory_dest_forward + j, test_memory_size);
+		}
+	}
+
+	for(int i = 0; i < test_iter; i++)
+	{
+		for(int j = 0; j < test_iter; j++)
+		{
+			init_rand_memory(test_memory_src + i, test_memory_size);
+			init_rand_memory(test_memory_dest_backward + j, test_memory_size);
+
+			printf("\nInitial : \n");
+			printf("src:");
+			print_memory(test_memory_src + i, test_memory_size);
+			printf("dest:");
+			print_memory(test_memory_dest_backward + j, test_memory_size);
+
+			memory_move(test_memory_dest_backward + j, test_memory_src + i, test_memory_size);
+
+			printf("Result : \n");
+			printf("dest:");
+			print_memory(test_memory_dest_backward + j, test_memory_size);
 		}
 	}
 
