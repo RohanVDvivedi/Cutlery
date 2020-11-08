b9e8d3cc8b6f5d8b0ae543f53d60fefda66f3fc5
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-11-08 13:01:59 +0530

    started to align test case code to the new dstring api

--- test/testDstring.c
+++ test/testDstring.c
@@ -6,65 +6,72 @@ int main()
 {
 	dstring str;
 	init_dstring(&str, "");
-	printf("str : ");display_dstring(&str);printf("\n");
+	printf("str : ");printf_dstring(&str);printf("\n");
 
 	append_to_dstring(&str, "Hello ");
-	printf("str : ");display_dstring(&str);printf("\n");
+	printf("str : ");printf_dstring(&str);printf("\n");
 
 	dstring tempstr;
 	init_dstring(&tempstr, "World, as said by Rohan AAACAAAAAC BBCBBD BBCBBD");
-	printf("tempstr : ");display_dstring(&tempstr);printf("\n");
+	printf("tempstr : ");printf_dstring(&tempstr);printf("\n");
 	concatenate_dstring(&str, &tempstr);
 	deinit_dstring(&tempstr);
-	printf("str : ");display_dstring(&str);printf("\n\n");
+	printf("str : ");printf_dstring(&str);printf("\n\n");
 
-	char* temp = NULL;
+	{
+		char* temp = "";
+		printf("prefix %s %d\n", temp, is_prefix(&str, dstring_DUMMY_CSTRING(temp)));
 
-	temp = "";
-	printf("prefix %s %d\n", temp, is_prefix(&str, temp));
-	temp = "H";
-	printf("prefix %s %d\n", temp, is_prefix(&str, temp));
-	temp = "Helt";
-	printf("prefix %s %d\n", temp, is_prefix(&str, temp));
-	temp = "Hell";
-	printf("prefix %s %d\n", temp, is_prefix(&str, temp));
-	temp = "Hello World";
-	printf("prefix %s %d\n", temp, is_prefix(&str, temp));
-	temp = "Hello Worlds";
-	printf("prefix %s %d\n", temp, is_prefix(&str, temp));
-	temp = "Hello World, as said by Rohan";
-	printf("prefix %s %d\n", temp, is_prefix(&str, temp));
-	temp = "Hello World, as said by Rohan, Hello World";
-	printf("prefix %s %d\n", temp, is_prefix(&str, temp));
+		temp = "H";
+		printf("prefix %s %d\n", temp, is_prefix(&str, dstring_DUMMY_CSTRING(temp)));
+		
+		temp = "Helt";
+		printf("prefix %s %d\n", temp, is_prefix(&str, dstring_DUMMY_CSTRING(temp)));
+		
+		temp = "Hell";
+		printf("prefix %s %d\n", temp, is_prefix(&str, dstring_DUMMY_CSTRING(temp)));
+		
+		temp = "Hello World";
+		printf("prefix %s %d\n", temp, is_prefix(&str, dstring_DUMMY_CSTRING(temp)));
+		
+		temp = "Hello Worlds";
+		printf("prefix %s %d\n", temp, is_prefix(&str, dstring_DUMMY_CSTRING(temp)));
+		
+		temp = "Hello World, as said by Rohan";
+		printf("prefix %s %d\n", temp, is_prefix(&str, dstring_DUMMY_CSTRING(temp)));
+		
+		temp = "Hello World, as said by Rohan, Hello World";
+		printf("prefix %s %d\n", temp, is_prefix(&str, dstring_DUMMY_CSTRING(temp)));
+	}
 
 	append_to_dstring_formatted(&str, "iNt no : %d, strIng : %s, floAT : %f", 336, "Heello Worlds!! =+", 35.49);
-	display_dstring(&str);
+	printf_dstring(&str);
 	printf("\n\n");
 
 	dstring slize;
 	printf("Created a slize : ");
 	init_dstring_slize(&slize, str.cstring + 4, 10);
-	display_dstring(&slize);
+	printf_dstring(&slize);
 	printf("\n\n");
 
 	printf("Created a slize : ");
 	init_dstring_slize(&slize, str.cstring, str.bytes_occupied);
-	display_dstring(&slize);
+	printf_dstring(&slize);
 	printf("\non compare to str = case_compare(str,slize) = %d\n\n", case_compare_dstring(&str, &slize));
 
 	printf("Created a slize : ");
 	init_dstring_slize(&slize, str.cstring, str.bytes_occupied);
-	display_dstring(&slize);
+	printf_dstring(&slize);
 	printf("\non compare to str = compare(str,slize) = %d\n\n", compare_dstring(&str, &slize));
 
 	printf("Created a slize : ");
 	init_dstring_slize(&slize, str.cstring, 20);
-	display_dstring(&slize);
+	printf_dstring(&slize);
 	printf("\non compare to str = case_compare(str,slize) = %d\n\n", case_compare_dstring(&str, &slize));
 
 	printf("Created a slize : ");
 	init_dstring_slize(&slize, str.cstring, 20);
-	display_dstring(&slize);
+	printf_dstring(&slize);
 	printf("\non compare to str = compare(str,slize) = %d\n\n", compare_dstring(&str, &slize));
 
 	init_dstring_slize(&slize, str.cstring, str.bytes_occupied);
@@ -74,7 +81,7 @@ int main()
 	toUppercase(&capitalize_slize);
 	deinit_dstring(&capitalize_slize);
 	printf("Created a slize : ");
-	display_dstring(&slize);
+	printf_dstring(&slize);
 	printf("\n\n");
 
 	printf("\non compare to str = case_compare(str,slize) = %d\n\n", case_compare_dstring(&str, &slize));
@@ -83,7 +90,7 @@ int main()
 
 	deinit_dstring(&slize);
 
-	display_dstring(&str);
+	printf_dstring(&str);
 	printf("\n\n");
 
 	printf("Testing contains function for \"%s\" in str => %d\n\n", "Hello World", contains_cstring(&str, "Hello World"));
@@ -98,41 +105,41 @@ int main()
 	printf("Testing contains function for \"%s\" in str => %d\n\n", "Rohhan", contains_cstring(&str, "Rohhan"));
 
 	printf("\n\nsubstring using std and KMP algorithm\n");
-	display_dstring(&str);
+	printf_dstring(&str);
 	printf("\n\n");
 
 	dstring substring1;init_dstring(&substring1, "AFFA");unsigned int* cache1 = alloca(sizeof(unsigned int) * (substring1.bytes_occupied+1));get_prefix_suffix_match_lengths(&substring1, cache1);
 	for(unsigned int i = 0; i <= substring1.bytes_occupied; i++)
 		printf("%u -> %u\n", i, cache1[i]);
-	printf("\nsubstring1 : \"");display_dstring(&substring1);printf("\"  O(m*n): %d, O(m+n): %d\n\n", contains_dstring(&str, &substring1, NULL), contains_dstring(&str, &substring1, cache1));
+	printf("\nsubstring1 : \"");printf_dstring(&substring1);printf("\"  O(m*n): %d, O(m+n): %d\n\n", contains_dstring(&str, &substring1, NULL), contains_dstring(&str, &substring1, cache1));
 	deinit_dstring(&substring1);
 
 	dstring substring2;init_dstring(&substring2, "BBCBBD");unsigned int* cache2 = alloca(sizeof(unsigned int) * (substring2.bytes_occupied+1));get_prefix_suffix_match_lengths(&substring2, cache2);
 	for(unsigned int i = 0; i <= substring2.bytes_occupied; i++)
 		printf("%u -> %u\n", i, cache2[i]);
-	printf("\nsubstring2 : \"");display_dstring(&substring2);printf("\"  O(m*n): %d, O(m+n): %d\n\n", contains_dstring(&str, &substring2, NULL), contains_dstring(&str, &substring2, cache2));
+	printf("\nsubstring2 : \"");printf_dstring(&substring2);printf("\"  O(m*n): %d, O(m+n): %d\n\n", contains_dstring(&str, &substring2, NULL), contains_dstring(&str, &substring2, cache2));
 	deinit_dstring(&substring2);
 
 	dstring substring3;init_dstring(&substring3, "AABAACAABAA");unsigned int* cache3 = alloca(sizeof(unsigned int) * (substring3.bytes_occupied+1));get_prefix_suffix_match_lengths(&substring3, cache3);
 	for(unsigned int i = 0; i <= substring3.bytes_occupied; i++)
 		printf("%u -> %u\n", i, cache3[i]);
-	printf("\nsubstring3 : \"");display_dstring(&substring3);printf("\"  O(m*n): %d, O(m+n): %d\n\n", contains_dstring(&str, &substring3, NULL), contains_dstring(&str, &substring3, cache3));
+	printf("\nsubstring3 : \"");printf_dstring(&substring3);printf("\"  O(m*n): %d, O(m+n): %d\n\n", contains_dstring(&str, &substring3, NULL), contains_dstring(&str, &substring3, cache3));
 	deinit_dstring(&substring3);
 
 	dstring substring4;init_dstring(&substring4, "AAACAAAAAC");unsigned int* cache4 = alloca(sizeof(unsigned int) * (substring4.bytes_occupied+1));get_prefix_suffix_match_lengths(&substring4, cache4);
 	for(unsigned int i = 0; i <= substring4.bytes_occupied; i++)
 		printf("%u -> %u\n", i, cache4[i]);
-	printf("\nsubstring4 : \"");display_dstring(&substring4);printf("\"  O(m*n): %d, O(m+n): %d\n\n", contains_dstring(&str, &substring4, NULL), contains_dstring(&str, &substring4, cache4));
+	printf("\nsubstring4 : \"");printf_dstring(&substring4);printf("\"  O(m*n): %d, O(m+n): %d\n\n", contains_dstring(&str, &substring4, NULL), contains_dstring(&str, &substring4, cache4));
 	deinit_dstring(&substring4);
 
 	toLowercase(&str);
 	printf("To lowercase str : ");
-	display_dstring(&str);
+	printf_dstring(&str);
 	printf("\n\n");
 
 	toUppercase(&str);
 	printf("To uppercase str : ");
-	display_dstring(&str);
+	printf_dstring(&str);
 	printf("\n\n");
 
 	deinit_dstring(&str);
