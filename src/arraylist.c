327bf1536f203b666e32fdfcdcc7f31f0ada65c4
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-11-14 17:44:19 +0530

    added declaration in the source file that need to be implemented

--- src/arraylist.c
+++ src/arraylist.c
@@ -1,1 +1,37 @@
 #include<arraylist.h>
+
+void intialize_arraylist(arraylist* al, unsigned int initial_size);
+
+int push_front(arraylist* al, const void* data_p);
+
+int push_back(arraylist* al, const void* data_p);
+
+int pop_front(arraylist* al);
+
+int pop_back(arraylist* al);
+
+const void* get_front(const arraylist* al);
+
+const void* get_back(const arraylist* al);
+
+const void* get_nth_from_front(const arraylist* al, unsigned int n);
+
+const void* get_nth_from_back(const arraylist* al, unsigned int n);
+
+unsigned int get_arraylist_element_count(const arraylist* al);
+
+int is_arraylist_full(const arraylist* al);
+
+int is_arraylist_empty(const arraylist* al);
+
+int expand_arraylist(arraylist* al);
+
+int shrink_arraylist(arraylist* al);
+
+const void* find_equals_in_arraylist(const arraylist* ll, const void* data, int (*compare)(const void* al_data, const void* data));
+
+void for_each_in_arraylist(const arraylist* al, void (*operation)(void* data_p, unsigned int index, const void* additional_params), const void* additional_params);
+
+void print_arraylist(const arraylist* al);
+
+void deintialize_arraylist(arraylist* al);
