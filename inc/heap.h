d877af5c8c8d594f6501740f58ed730f51d086a8
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-11-25 10:57:41 +0530

    changes to api for the following

--- inc/heap.h
+++ inc/heap.h
@@ -40,14 +40,19 @@ struct heap
 void initialize_heap(heap* heap_p, unsigned int expected_size, heap_type type, int (*compare)(const void* data1, const void* data2), void (*heap_index_update_callback)(const void* data, unsigned int heap_index, const void* callback_params), const void* callback_params);
 
 // push a new data element to the heap
+// push returns 1, if data_p is successfully pushed
+// push returns 0 and fails if the heap container is full
+// if push fails you must expand_heap before pushing any new elements
 // O(log(N)) operation
-void push_heap(heap* heap_p, const void* data);
+int push_heap(heap* heap_p, const void* data);
 
-// returns pointer to the minimum data of the heap, returns NULL if the heap is empty
+// pop the top element from the heap
+// pop returns 1, if an element is successfully popped
+// pop returns 0 and fails if the heap container is already empty and does not have any elements to pop
+int pop_heap(heap* heap_p);
+
+// returns pointer to the top data element of the heap, returns NULL if the heap is empty
 const void* get_top_heap(const heap* heap_p);
-
-// pop the top element from the heap, no element will be popped if the heap is already empty
-void pop_heap(heap* heap_p);
 
 // the below function is to be called when you doubt if the heap properties are being maintained at the given index
 // or because you changed the attributes of the data, which changed its ordering
@@ -60,12 +65,29 @@ void heapify_at(heap* heap_p, unsigned int index);
 // i.e. fails if index is out of bounds, else if the element was removed it will return 1
 int remove_from_heap(heap* heap_p, unsigned int index);
 
+void deinitialize_heap(heap* heap_p);
+
+// expand the heap container, returns 1 on successfull expansion of the heap container
+// else returns 0
+int expand_heap(heap* heap_p);
+
+// shrink the heap container, returns 1 on successfull shrink of the heap container
+// else returns 0
+// you may call this function at regular intervals, to keep the heap size in check
+int shrink_heap(heap* heap_p);
+
+// returns 1, if the heap is empty, i.e. does not have any elements to pop
+// else it return 0
+int is_empty_heap(heap* heap_p);
+
+// returns 1, if the heap container is full, i.e. it needs to be expanded to push any futher elements
+// else it return 0, this means there is still space in the container to push elements
+int is_full_heap(heap* heap_p);
+
 // perform an operation on all the key value pairs for the heap
 void for_each_in_heap(const heap* heap_p, void (*operation)(void* data, unsigned int heap_index, const void* additional_params), const void* additional_params);
 
 // print, heap and all the elements of the heap
 void print_heap(heap* heap_p, void (*print_element)(const void* data));
-
-void deinitialize_heap(heap* heap_p);
 
 #endif
--- inc/queue.h
+++ inc/queue.h
@@ -24,23 +24,40 @@ struct queue
 void initialize_queue(queue* queue_p, unsigned int expected_size);
 
 // push a new element to the queue
-void push_queue(queue* queue_p, const void* data_p);
+// push returns 1, if data_p is successfully pushed
+// push returns 0 and fails if the queue container is full
+// if push fails you must expand_queue before pushing any new elements
+int push_queue(queue* queue_p, const void* data_p);
 
 // pop the element from the queue which was inserted earliest of all other elements
-void pop_queue(queue* queue_p);
+// pop returns 1, if an element is successfully popped
+// pop returns 0 and fails if the queue container is already empty and does not have any elements to pop
+int pop_queue(queue* queue_p);
 
 // returns pointer to the top element data pointer, (i.e. data pointer to the earliest inserted element) 
 const void* get_top_queue(queue* queue_p);
+const void* get_nth_from_top_queue(queue* queue_p, unsigned int n);
 
 // frees all the data being held by the queue, this function, does not release memory of the actual queue structure, only the memory of the components
 // the same queue can be reused by calling initialize_queue function, after it is deinitialized
 void deinitialize_queue(queue* queue_p);
 
-// if the queue is empty
-int isQueueEmpty(queue* queue_p);
+// expand the queue container, returns 1 on successfull expansion of the queue container
+// else returns 0
+int expand_queue(queue* queue_p);
 
-// if the queue_holder is full and needs expansion
-int isQueueHolderFull(queue* queue_p);
+// shrink the queue container, returns 1 on successfull shrink of the queue container
+// else returns 0
+// you may call this function at regular intervals, to keep the queue size in check
+int shrink_queue(queue* queue_p);
+
+// returns 1, if the queue is empty, i.e. does not have any elements to pop
+// else it return 0
+int is_empty_queue(queue* queue_p);
+
+// returns 1, if the queue container is full, i.e. it needs to be expanded to push any futher elements
+// else it return 0, this means there is still space in the container to push elements
+int is_full_queue(queue* queue_p);
 
 // for each function, to iteration over all non null elements to perform an operation
 void for_each_in_queue(const queue* queue_p, void (*operation)(void* data_p, unsigned int index, const void* additional_params), const void* additional_params);
