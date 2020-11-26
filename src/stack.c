b5eab3a5699459db4e9f3c8f2046e191a4b4ed90
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-11-26 10:10:23 +0530

    stack implementation for arraylist completed

--- src/stack.c
+++ src/stack.c
@@ -32,17 +32,35 @@ void deinitialize_stack(stack* stack_p)
 	deinitialize_arraylist(&(stack_p->stack_holder));
 }
 
-unsigned int get_total_size_stack(stack* stack_p);
+unsigned int get_total_size_stack(stack* stack_p)
+{
+	return get_total_size_arraylist(&(stack_p->stack_holder));
+}
 
-unsigned int get_element_count_stack(stack* stack_p);
+unsigned int get_element_count_stack(stack* stack_p)
+{
+	return get_element_count_arraylist(&(stack_p->stack_holder));
+}
 
-int is_full_stack(stack* stack_p);
+int is_full_stack(stack* stack_p)
+{
+	return is_full_arraylist(&(stack_p->stack_holder));
+}
 
-int is_empty_stack(stack* stack_p);
+int is_empty_stack(stack* stack_p)
+{
+	return is_empty_arraylist(&(stack_p->stack_holder));
+}
 
-int expand_stack(stack* stack_p);
+int expand_stack(stack* stack_p)
+{
+	return expand_arraylist(&(stack_p->stack_holder));
+}
 
-int shrink_stack(stack* stack_p);
+int shrink_stack(stack* stack_p)
+{
+	return shrink_arraylist(&(stack_p->stack_holder));
+}
 
 void for_each_in_stack(const stack* stack_p, void (*operation)(void* data_p, unsigned int index, const void* additional_params), const void* additional_params)
 {
