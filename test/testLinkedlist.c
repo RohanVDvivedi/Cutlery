e31b4b1e87ef10fc4c3c2b5588e46b73b1f0504d
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-11-27 09:30:29 +0530

    fixed test cases for linkedlist api changes

--- test/testLinkedlist.c
+++ test/testLinkedlist.c
@@ -38,6 +38,8 @@ int main()
 	linkedlist* ll = &llist;
 	initialize_linkedlist(ll, (unsigned long long int)(&(((ts*)0)->ll_embed_node)));
 
+	printf("Error on attempt to remove head : %d, or tail : %d, from an empty linkedlist\n\n", remove_head(ll), remove_tail(ll));
+
 	print_linkedlist(ll, print_ts);
 
 	insert_head(ll, &((ts){2, "two", {NULL, NULL}}));
@@ -71,10 +73,10 @@ int main()
 
 	const ts* temp = get_nth_from_head(ll, 2);
 
-	remove_from_list(ll, temp);
+	remove_from_linkedlist(ll, temp);
 	print_linkedlist(ll, print_ts);
 
-	printf("Error on removing a not existing node twice %d\n", remove_from_list(ll, temp));
+	printf("Error on removing a not existing node twice %d\n", remove_from_linkedlist(ll, temp));
 	print_linkedlist(ll, print_ts);
 
 	printf("Error on inserting before a not existing node %d\n", insert_before(ll, temp, &((ts){-1, "minus one", {NULL, NULL}})));
@@ -87,27 +89,27 @@ int main()
 	print_linkedlist(ll, print_ts);
 
 	printf("Searching for structure with a = 4\n");
-	print_ts(find_equals_in_list(ll, &((ts){4}), test_compare));
+	print_ts(find_equals_in_linkedlist(ll, &((ts){4}), test_compare));
 	printf("\n");
 
 	printf("Searching for structure with a = 1\n");
-	print_ts(find_equals_in_list(ll, &((ts){1}), test_compare));
+	print_ts(find_equals_in_linkedlist(ll, &((ts){1}), test_compare));
 	printf("\n");
 
 	printf("Searching for structure with a = 2\n");
-	print_ts(find_equals_in_list(ll, &((ts){2}), test_compare));
+	print_ts(find_equals_in_linkedlist(ll, &((ts){2}), test_compare));
 	printf("\n");
 
 	printf("Searching for structure with a = -1\n");
-	print_ts(find_equals_in_list(ll, &((ts){-1}), test_compare));
+	print_ts(find_equals_in_linkedlist(ll, &((ts){-1}), test_compare));
 	printf("\n");
 
 	printf("Searching for structure with a = 6\n");
-	print_ts(find_equals_in_list(ll, &((ts){6}), test_compare));
+	print_ts(find_equals_in_linkedlist(ll, &((ts){6}), test_compare));
 	printf("\n");
 
 	printf("Searching for structure with a = 5\n");
-	print_ts(find_equals_in_list(ll, &((ts){5}), test_compare));
+	print_ts(find_equals_in_linkedlist(ll, &((ts){5}), test_compare));
 	printf("\n");
 
 	return 0;
