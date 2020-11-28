0379b2704f31b57a1d6095321bc7fa44c3590d3a
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-11-28 16:13:11 +0530

    implemented backward / rever copy for memory move, that uses int copy

--- src/cutlery_stds.c
+++ src/cutlery_stds.c
@@ -23,11 +23,11 @@ void memory_move(void* dest_start, const void* src_start, unsigned int size)
 	// based on whether the pass could corrupt the src memory address before we could copy it
 
 	// make forward pass
-	if(dest_start < src_start || src_last < dest_start)
+	if( dest_start < src_start )
 	{
 		// intialize our iterators for forward copy
-		char* dest = dest_start;
 		const char* src = src_start;
+		char* dest = dest_start;
 
 		// check if int copy is possible, and requires atleast 3 iterations (atleast 2 int copy iterations)
 		if( (size >= 3 * int_size) &&
@@ -37,15 +37,15 @@ void memory_move(void* dest_start, const void* src_start, unsigned int size)
 		)
 		{
 			// perform a byte-by-byte copy until the addresses are int aligned
-			while( ((long int)src) & ~int_alignment_bit_mask )
+			while( ( ((long int)src) & ~int_alignment_bit_mask ) )
 				*(dest++) = *(src++);
 
 			// perform an int-by-int transfer in this scope
 			// dest_int and src_int must not leave this scope
 			// they must equal the dest and src, right before and right after this scope
 			{
-				int* dest_int = (int*)dest;
 				const int* src_int = (int*)src;
+				int* dest_int = (int*)dest;
 
 				// additonal bytes that you might have to copy after completing the int copy
 				unsigned long int additional_bytes = ((unsigned long int)(src_last + 1)) & ~int_alignment_bit_mask;
@@ -57,8 +57,8 @@ void memory_move(void* dest_start, const void* src_start, unsigned int size)
 				while( src_int <= src_last_int )
 					*(dest_int++) = *(src_int++);
 
-				dest = (char*)dest_int;
 				src = (char*)src_int;
+				dest = (char*)dest_int;
 			}
 		}
 
@@ -70,9 +70,50 @@ void memory_move(void* dest_start, const void* src_start, unsigned int size)
 	else
 	{
 		// intialize our iterators for backward copy
-		char* dest = dest_last;
 		const char* src = src_last;
+		char* dest = dest_last;
 
+		// check if int copy is possible, and requires atleast 3 iterations (atleast 2 int copy iterations)
+		if( (size >= 3 * int_size) &&
+			(
+				(((long int)src_start) & ~int_alignment_bit_mask) == (((long int)dest_start) & ~int_alignment_bit_mask)
+			)
+		)
+		{
+			// perform a byte-by-byte copy until the addresses are int - 1 aligned
+			while( ( ((long int)src) & ~int_alignment_bit_mask ) != ( ~int_alignment_bit_mask ) )
+				*(dest--) = *(src--);
+
+			// point src and dest to next int that needs to be copied
+			src -= (int_size - 1);
+			dest -= (int_size - 1);
+
+			// perform an int-by-int transfer in this scope
+			// dest_int and src_int must not leave this scope
+			// they must equal the dest and src, right before and right after this scope
+			{
+				const int* src_int = (int*)src;
+				int* dest_int = (int*)dest;
+
+				// additonal bytes that you might have to copy after completing the int copy
+				unsigned long int additional_bytes = 4 - (((unsigned long int)(src_start)) & ~int_alignment_bit_mask);
+
+				// this is the address of the last byte that must be copied under the int-by-int copy loop
+				const int* src_start_int = src_start + additional_bytes;
+
+				// int-by-int copy loop
+				while( src_int >= src_start_int )
+					*(dest_int--) = *(src_int--);
+
+				src = ((char*)src_int) + (int_size - 1);
+				dest = ((char*)dest_int) + (int_size - 1);
+			}
+
+			src += (int_size - 1);
+			dest += (int_size - 1);
+		}
+
+		// finish up remaining with an old fashioned byte-by-byte copy loop
 		while( src >= ((char*)(src_start)) )
 			*(dest--) = *(src--);
 	}
@@ -94,7 +135,7 @@ void memory_set(void* dest_start, char byte_value, unsigned int size)
 	if(size >= 3 * int_size)
 	{
 		// perform a byte-by-byte copy until the address is int aligned
-		while( ((long int)dest) & ~int_alignment_bit_mask )
+		while( ( ((long int)dest) & ~int_alignment_bit_mask ) )
 			*(dest++) = byte_value;
 
 		// perform an int-by-int transfer in this scope
