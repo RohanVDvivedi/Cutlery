b9e123483366617381dfc7e9789fd45f58bfd0a4
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-12-05 10:57:21 +0530

    more memory control to the client, shrinking happens under complete client control unless, the client wants to shrink to size 0, which is not possible

--- inc/array.h
+++ inc/array.h
@@ -29,12 +29,13 @@ void deinitialize_array(array* array_p);
 
 // it expands array
 // it returns 1, for success if the array container was successfull expanded,
-// else this function may fail with 0, to indicate a failure, fron the memory allocator
+// else this function may fail with 0, to indicate a failure, from the memory allocator
 int expand_array(array* array_p);
 
 // shrinks the array, if the array is considerably larger than the minimum size ( = end_index - start_index + 1) that was required
 // element at the start_index comes to 0, and element at end_index comes to end_index - start_index
 // it returns 1 if the array was shrunk, else it will return a 0 for a failure
+// and since the start_index <= end_index, this funtion will not shrink your array to size 0
 int shrink_array(array* array_p, unsigned int start_index, unsigned int end_index);
 
 // perform operation on all the elements of the array, the method operation takes in 2 params, the data_p to operation on and its index
