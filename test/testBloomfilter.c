feaedb865984ffb719c98fd4ca39b38e65ceecc3
Rohan Dvivedi <rohandvivedi@gmail.com>
2021-03-01 12:25:58 +0530

    added a bitmap test case for use to build bloomfilter

--- test/testBloomfilter.c
+++ test/testBloomfilter.c
@@ -17,7 +17,10 @@ unsigned int hash_f_2(char* str)
 {
 	unsigned int sum_chars = 0;
 	while((*str) != '\0')
-		sum_chars += ((unsigned int)(*str));
+	{
+		sum_chars += (((unsigned int)(*str)) - ((('a'<=((unsigned int)(*str)))&&(((unsigned int)(*str))<='z'))?'a':'A'));
+		str++;
+	}
 	return sum_chars % BLOOM_FILTER_SIZE_IN_BITS;
 }
 
@@ -30,17 +33,43 @@ typedef enum find_result find_result;
 
 void insert_in_bloomfilter(const char* str)
 {
-	set_bit(bloomfilter_bitmap[0], hash_f_1(str));
-	set_bit(bloomfilter_bitmap[1], hash_f_2(str));
+	unsigned int hash1 = hash_f_1(str);
+	unsigned int hash2 = hash_f_2(str);
+	printf("Bloom filter insert : %s\n\thash1 : %u\n\thash2 : %u\n\n", str, hash1, hash2);
+	set_bit(bloomfilter_bitmap[0], hash1);
+	set_bit(bloomfilter_bitmap[1], hash2);
 }
 
 find_result check_if_inserted_in_bloomfilter(const char* str)
 {
-	return ( get_bit(bloomfilter_bitmap[0], hash_f_1(str)) 
-		& get_bit(bloomfilter_bitmap[1], hash_f_2(str)) )  ? MAY_BE_PRESENT : NOT_PRESENT;
+	find_result fresult = get_bit(bloomfilter_bitmap[0], hash_f_1(str)) 
+						& get_bit(bloomfilter_bitmap[1], hash_f_2(str));
+	printf("Bloom filter check  : %s : %s\n\n", str, (fresult?"MAY_BE_PRESENT":"NOT_PRESENT"));
+	return fresult ? MAY_BE_PRESENT : NOT_PRESENT;
+}
+
+void print_bloomfilter()
+{
+	printf("bitmap 0 : ");
+	print_bitmap(bloomfilter_bitmap[0], BLOOM_FILTER_SIZE_IN_BITS);
+	printf("bitmap 1 : ");
+	print_bitmap(bloomfilter_bitmap[1], BLOOM_FILTER_SIZE_IN_BITS);
+	printf("\n\n");
 }
 
 int main()
 {
+	insert_in_bloomfilter("hello");
 
+	print_bloomfilter();
+
+	check_if_inserted_in_bloomfilter("hello");
+
+	check_if_inserted_in_bloomfilter("eeloo");
+
+	check_if_inserted_in_bloomfilter("rohan");
+
+	check_if_inserted_in_bloomfilter("dvivedi");
+
+	return 0;
 }
