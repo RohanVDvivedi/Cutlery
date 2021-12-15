567fa7ca05cd581e0cea9e59a3f4ee93b43bae33
rohan <rohandvivedi@gmail.com>
2021-12-15 18:44:57 +0530

    refactored header file to use last_index instead of end_index

--- inc/array_search_sort.h
+++ inc/array_search_sort.h
@@ -17,22 +17,22 @@ typedef struct array array;
 //                         SORT FUNCTIONS ON ARRAY
 //---------------------------------------------------------------------------
 
-// All functions sort elements in array from start_index to end_index, both inclusive,
+// All functions sort elements in array from start_index to last_index, both inclusive,
 // the sorting is based on the compare function provided
 
 
 // sorting algorithm used => merge sort algorithm (iterative approach)
-void merge_sort_array(array* array_p, unsigned int start_index, unsigned int end_index, int (*compare)(const void* data1, const void* data2));
+void merge_sort_array(array* array_p, unsigned int start_index, unsigned int last_index, int (*compare)(const void* data1, const void* data2));
 
 // sorting algorithm used => heap sort algorithm
-void heap_sort_array(array* array_p, unsigned int start_index, unsigned int end_index, int (*compare)(const void* data1, const void* data2));
+void heap_sort_array(array* array_p, unsigned int start_index, unsigned int last_index, int (*compare)(const void* data1, const void* data2));
 
 // sorting algorithm used => quick sort algorithm
-void quick_sort_array(array* array_p, unsigned int start_index, unsigned int end_index, int (*compare)(const void* data1, const void* data2));
+void quick_sort_array(array* array_p, unsigned int start_index, unsigned int last_index, int (*compare)(const void* data1, const void* data2));
 
 // sorting algorithm used => radix sort algorithm
 // all the elements will be ordered in increasing order of their value for get_sort_attribute()
-void radix_sort_array(array* array_p, unsigned int start_index, unsigned int end_index, unsigned int (*get_sort_attribute)(const void* data));
+void radix_sort_array(array* array_p, unsigned int start_index, unsigned int last_index, unsigned int (*get_sort_attribute)(const void* data));
 
 
 
@@ -45,7 +45,6 @@ void radix_sort_array(array* array_p, unsigned int start_index, unsigned int end
 
 // These functions return an index to the array element, that compares equals to the data provided
 // The data provided as parameter for equality checking may or may not be pointing to the element, already existing in array
-// if the element is not found an INVALID_INDEX is returned
 // the search_occurence parameter suggests whether we are looking for first or the last occurence that compares equal
 
 
@@ -56,23 +55,21 @@ enum search_occurence
 };
 typedef enum search_occurence search_occurence;
 
-// performs linear search in array from start_index to end_index, both inclusive,
-// returns INVALID_INDEX, if the element is not found
-unsigned int linear_search_in_array(const array* array_p, unsigned int start_index, unsigned int end_index, const void* data, int (*compare)(const void* data1, const void* data2), search_occurence occurence_type);
+// performs linear search in array from start_index to last_index, both inclusive,
+// returns INVALID_INDEX, if the element is not found, or the indexes provided are invalid
+unsigned int linear_search_in_array(const array* array_p, unsigned int start_index, unsigned int last_index, const void* data, int (*compare)(const void* data1, const void* data2), search_occurence occurence_type);
 
 
 
 
-// NOTE NOTE NOTE
+// NOTE :::
 // the binary_search_in_array function can be called on the array, only if
 // your data in the array is sorted in ascending order in accordance to the compare function provided as parameter
-// , (sorted) for all indices from start_index to end_index inclusive
+// , (sorted) for all indices from start_index to last_index, both inclusive
 
-// performs binary search in array from start_index to end_index, both inclusive,
-// it returns the element that when compared to data equals to 0
-// if no such element is found, we return INVALID_INDEX
-// if the indexes provided are inappropriate to begin with, the function returns INVALID_INDEX
-unsigned int binary_search_in_array(const array* array_p, unsigned int start_index, unsigned int end_index, const void* data, int (*compare)(const void* data1, const void* data2), search_occurence occurence_type);
+// performs binary search in array from start_index to last_index, both inclusive,
+// returns INVALID_INDEX, if the element is not found, or the indexes provided are invalid
+unsigned int binary_search_in_array(const array* array_p, unsigned int start_index, unsigned int last_index, const void* data, int (*compare)(const void* data1, const void* data2), search_occurence occurence_type);
 
 
 #endif
