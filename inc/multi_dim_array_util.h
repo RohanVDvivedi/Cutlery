3f7cf6624592f5d6a64f38bcf3b917f92dde5e82
rohan <rohandvivedi@gmail.com>
2021-12-17 21:50:17 +0530

    added comments and implemented indices to accessor functionality

--- inc/multi_dim_array_util.h
+++ inc/multi_dim_array_util.h
@@ -1,12 +1,21 @@
 #ifndef MULTI_DIM_ARRAY_UTIL_H
 #define MULTI_DIM_ARRAY_UTIL_H
 
+// total number of elements in multi dimension array
 unsigned int get_element_count_in_multi_dim_array(const unsigned int* dims, unsigned int dims_count);
 
-int get_accessor_from_indices(unsigned int* accessor, const unsigned int* indices, const unsigned int* dims, unsigned int dims_count);
+// accessor to array[index[dims_count-1]]...[index[1]][index[0]]
+// is given by array[accessor]
+// please make sure that all the indices are in range
+// i.e. for any i form 0 to dims_count - 1 both inclusive ==>>> 0 <= index[i] < dims[i]
+unsigned int get_accessor_from_indices(const unsigned int* indices, const unsigned int* dims, unsigned int dims_count);
 
 unsigned int get_max_index_in_multi_dim_array(unsigned int dim_no, const unsigned int* dims, unsigned int dims_count);
 
-int get_indices_from_accessor(unsigned int* indices, unsigned int accessor, const unsigned int* dims, unsigned int dims_count);
+// accessor to array[index[dims_count-1]]...[index[1]][index[0]]
+// is given by array[accessor]
+// please make sure that the accessor in in range
+// i.e. 0 <= accessor < ( dims[dims_count - 1] * ... * dims[1] * dims[0] )
+void get_indices_from_accessor(unsigned int* indices, unsigned int accessor, const unsigned int* dims, unsigned int dims_count);
 
 #endif
--- src/multi_dim_array_util.c
+++ src/multi_dim_array_util.c
@@ -1,9 +1,28 @@
 #include<multi_dim_array_util.h>
 
-unsigned int get_element_count_in_multi_dim_array(const unsigned int* dims, unsigned int dims_count);
+unsigned int get_element_count_in_multi_dim_array(const unsigned int* dims, unsigned int dims_count)
+{
+	unsigned int element_count = 0;
+	for(unsigned int dim = 0; dim < dims_count; dim++)
+		element_count *= dims[dim];
+	return element_count;
+}
 
-int get_accessor_from_indices(unsigned int* accessor, const unsigned int* indices, const unsigned int* dims, unsigned int dims_count);
+unsigned int get_accessor_from_indices(const unsigned int* indices, const unsigned int* dims, unsigned int dims_count)
+{
+	unsigned int accessor = 0;
+	unsigned int elements_under_ith_dimension = 1;
+	for(unsigned int dim = 0; dim < dims_count; dim++)
+	{
+		accessor += (elements_under_ith_dimension * dim[indices[dim]]);
+		elements_under_ith_dimension *= dims[dim];
+	}
+	return accessor;
+}
 
-unsigned int get_max_index_in_multi_dim_array(unsigned int dim_no, const unsigned int* dims, unsigned int dims_count);
+unsigned int get_max_index_in_multi_dim_array(unsigned int dim_no, const unsigned int* dims, unsigned int dims_count)
+{
+	return dims[dim_no] - 1;
+}
 
 int get_indices_from_accessor(unsigned int* indices, unsigned int accessor, const unsigned int* dims, unsigned int dims_count);
