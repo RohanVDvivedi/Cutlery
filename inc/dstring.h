66bfab5b3475ddab08be85391eaf25f160e91267
rohan <rohandvivedi@gmail.com>
2021-12-17 14:14:42 +0530

    added literal dstring macro

--- inc/dstring.h
+++ inc/dstring.h
@@ -21,6 +21,10 @@ void init_empty_dstring(dstring* str_p, unsigned int init_size);
 
 dstring get_dstring(const char* data, unsigned int data_size);
 
+#define get_literal_dstring(data, data_size) \
+	((const dstring){.byte_array = (data), .bytes_occupied = (data_size), .bytes_allocated = 0})
+#define get_literal_cstring(cstr) get_literal_dstring((cstr), strlen((cstr)))
+
 char* get_byte_array_dstring(const dstring* str_p);
 unsigned int get_char_count_dstring(const dstring* str_p);
 unsigned int get_capacity_dstring(const dstring* str_p);
