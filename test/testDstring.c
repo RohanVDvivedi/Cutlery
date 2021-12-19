4ad59dfc644ef206d9a691b6d9e7797aee0d82f6
rohan <rohandvivedi@gmail.com>
2021-12-19 14:37:12 +0530

    updated testcases for easy of addding test cases in temp

--- test/testDstring.c
+++ test/testDstring.c
@@ -5,122 +5,105 @@
 int main()
 {
 	dstring str = get_dstring(NULL, 0);
-	printf("str : ");printf_dstring(&str);printf("\n");
+	printf("str : \"" printf_dstring_format "\"\n", printf_dstring_params(&str));
 
 	concatenate_dstring(&str, &get_literal_cstring("Hello "));
-	printf("str : ");printf_dstring(&str);printf("\n");
+	printf("str : \"" printf_dstring_format "\"\n", printf_dstring_params(&str));
 
 	{
 		const dstring* tempstr = &get_literal_cstring("World, as said by Rohan AAACAAAAAC BBCBBD BBCBBD");
-		printf("concatenate str with tempstr : ");printf_dstring(tempstr);printf("\n");
+		printf("concatenate str with tempstr : \"" printf_dstring_format "\"\n", printf_dstring_params(tempstr));
 		concatenate_dstring(&str, tempstr);
-		printf("str : ");printf_dstring(&str);printf("\n\n");
+		printf("str : \"" printf_dstring_format "\"\n", printf_dstring_params(&str));
 	}
 
 	{
-		char* temp = "";
-		printf("prefix %s %d\n", temp, is_prefix(&str, &get_literal_cstring(temp)));
+		char* temp[] = {
+			"",
+			"H",
+			"Helt",
+			"Hell",
+			"Hello World",
+			"Hello Worlds",
+			"Hello World, as said by Rohan",
+			"Hello World, as said by Rohan, Hello World",
+		};
 
-		temp = "H";
-		printf("prefix %s %d\n", temp, is_prefix(&str, &get_literal_cstring(temp)));
-		
-		temp = "Helt";
-		printf("prefix %s %d\n", temp, is_prefix(&str, &get_literal_cstring(temp)));
-		
-		temp = "Hell";
-		printf("prefix %s %d\n", temp, is_prefix(&str, &get_literal_cstring(temp)));
-		
-		temp = "Hello World";
-		printf("prefix %s %d\n", temp, is_prefix(&str, &get_literal_cstring(temp)));
-		
-		temp = "Hello Worlds";
-		printf("prefix %s %d\n", temp, is_prefix(&str, &get_literal_cstring(temp)));
-		
-		temp = "Hello World, as said by Rohan";
-		printf("prefix %s %d\n", temp, is_prefix(&str, &get_literal_cstring(temp)));
-		
-		temp = "Hello World, as said by Rohan, Hello World";
-		printf("prefix %s %d\n", temp, is_prefix(&str, &get_literal_cstring(temp)));
+		int temp_size = sizeof(temp) / sizeof(char*);
+
+		for(int i = 0; i < temp_size; i++)
+			printf("prefix %s %d\n", temp[i], is_prefix(&str, &get_literal_cstring(temp[i])));
 	}
 
 	snprintf_dstring(&str, "iNt no : %d, strIng : %s, floAT : %f", 336, "Heello Worlds!! =+", 35.49);
-	printf_dstring(&str);
+	printf("str : \"" printf_dstring_format "\"\n", printf_dstring_params(&str));
 	printf("\n\n");
 
-	printf("Created a literal dstring : ");
-	printf_dstring(&get_literal_dstring(get_byte_array_dstring(&str) + 4, 10));
-	printf("\n\n");
+	printf("Created a literal dstring : " printf_dstring_format "\n\n", printf_dstring_params(&get_literal_dstring(get_byte_array_dstring(&str) + 4, 10)));
 
-	printf("Created a slize : ");
 	const dstring* slize = &get_literal_dstring(get_byte_array_dstring(&str), get_char_count_dstring(&str));
-	printf_dstring(slize);
-	printf("\non compare to str = case_compare(str,slize) = %d\n\n", case_compare_dstring(&str, slize));
+	printf("Created a literal dstring (slize) : \"" printf_dstring_format "\"\n", printf_dstring_params(slize));
+	printf("on compare to str = case_compare(str,slize) = %d\n\n", case_compare_dstring(&str, slize));
 
-	printf("Created a slize : ");
 	slize = &get_literal_dstring(get_byte_array_dstring(&str), get_char_count_dstring(&str));
-	printf_dstring(slize);
-	printf("\non compare to str = compare(str,slize) = %d\n\n", compare_dstring(&str, slize));
+	printf("Created a literal dstring (slize) : \"" printf_dstring_format "\"\n", printf_dstring_params(slize));
+	printf("on compare to str = compare(str,slize) = %d\n\n", compare_dstring(&str, slize));
 
-	printf("Created a slize : ");
 	slize = &get_literal_dstring(get_byte_array_dstring(&str), 20);
-	printf_dstring(slize);
-	printf("\non compare to str = case_compare(str,slize) = %d\n\n", case_compare_dstring(&str, slize));
+	printf("Created a literal dstring (slize) : \"" printf_dstring_format "\"\n", printf_dstring_params(slize));
+	printf("on compare to str = case_compare(str,slize) = %d\n\n", case_compare_dstring(&str, slize));
 
-	printf("Created a slize : ");
 	slize = &get_literal_dstring(get_byte_array_dstring(&str), 20);
-	printf_dstring(slize);
-	printf("\non compare to str = compare(str,slize) = %d\n\n", compare_dstring(&str, slize));
+	printf("Created a literal dstring (slize) : \"" printf_dstring_format "\"\n", printf_dstring_params(slize));
+	printf("on compare to str = compare(str,slize) = %d\n\n", compare_dstring(&str, slize));
 
-	printf("Created a slize : ");
 	slize = &get_literal_dstring(get_byte_array_dstring(&str), get_char_count_dstring(&str));
-	printf_dstring(slize);
-	printf("\n\n");
+	printf("Created a literal dstring (slize) : \"" printf_dstring_format "\"\n", printf_dstring_params(slize));
+	printf("on compare to str = case_compare(str,slize) = %d\n", case_compare_dstring(&str, slize));
+	printf("on compare to str = compare(str,slize) = %d\n\n", compare_dstring(&str, slize));
 
-	printf("\non compare to str = case_compare(str,slize) = %d\n\n", case_compare_dstring(&str, slize));
-	printf("\non compare to str = compare(str,slize) = %d\n\n", compare_dstring(&str, slize));
+	slize = &((dstring){});
+	init_dstring(slize, get_byte_array_dstring(&str), get_char_count_dstring(&str));
+	toUppercase(slize);
+	printf("Created an uppercase dstring (slize) : \"" printf_dstring_format "\"\n", printf_dstring_params(slize));
+	printf("on compare to str = case_compare(str,slize) = %d\n", case_compare_dstring(&str, slize));
+	printf("on compare to str = compare(str,slize) = %d\n\n", compare_dstring(&str, slize));
+	deinit_dstring(slize);
 
-	printf("Created a slize : ");
-	dstring* slize2 = &((dstring){});init_dstring(slize2, get_byte_array_dstring(&str), get_char_count_dstring(&str));
-	toUppercase(slize2);
-	printf_dstring(slize2);
-	printf("\n\n");
-	printf("\non compare to str = case_compare(str,slize2) = %d\n\n", case_compare_dstring(&str, slize2));
-	printf("\non compare to str = compare(str,slize2) = %d\n\n", compare_dstring(&str, slize2));
-	deinit_dstring(slize2);
+	printf("\n\nCONTAINS_DSTRING algorithms with str\n\n");
 
-	printf_dstring(&str);
-	printf("\n\n");
+	printf("str : " printf_dstring_format "\n", printf_dstring_params(&str));
 
 	{
-		char* temp = "Hello World";
-		printf("Testing contains function for \"%s\" in str => %p\n\n", temp, contains_dstring(&str, &get_literal_cstring(temp), NULL));
+		char* temp[] = {
+			"Hello World",
+			"Rohan",
+			"said by",
+			"said byt",
+			"rHello World",
+			"sRohan",
+			"Hello Worldp",
+			"Rohano",
+			"Hellyo World",
+			"Rohhan",
+		};
 
-		temp = "Rohan";
-		printf("Testing contains function for \"%s\" in str => %p\n\n", temp, contains_dstring(&str, &get_literal_cstring(temp), NULL));
-		
-		temp = "said by";
-		printf("Testing contains function for \"%s\" in str => %p\n\n", temp, contains_dstring(&str, &get_literal_cstring(temp), NULL));
-		
-		temp = "said byt";
-		printf("Testing contains function for \"%s\" in str => %p\n\n", temp, contains_dstring(&str, &get_literal_cstring(temp), NULL));
-		
-		temp = "rHello World";
-		printf("Testing contains function for \"%s\" in str => %p\n\n", temp, contains_dstring(&str, &get_literal_cstring(temp), NULL));
-		
-		temp = "sRohan";
-		printf("Testing contains function for \"%s\" in str => %p\n\n", temp, contains_dstring(&str, &get_literal_cstring(temp), NULL));
-		
-		temp = "Hello Worldp";
-		printf("Testing contains function for \"%s\" in str => %p\n\n", temp, contains_dstring(&str, &get_literal_cstring(temp), NULL));
-		
-		temp = "Rohano";
-		printf("Testing contains function for \"%s\" in str => %p\n\n", temp, contains_dstring(&str, &get_literal_cstring(temp), NULL));
-		
-		temp = "Hellyo World";
-		printf("Testing contains function for \"%s\" in str => %p\n\n", temp, contains_dstring(&str, &get_literal_cstring(temp), NULL));
-		
-		temp = "Rohhan";
-		printf("Testing contains function for \"%s\" in str => %p\n\n", temp, contains_dstring(&str, &get_literal_cstring(temp), NULL));
+		int temp_size = sizeof(temp) / sizeof(char*);
+
+		for(int i = 0; i < temp_size; i++)
+		{
+			printf("contains_dstring : %s\n", temp[i]);
+
+			dstring* temp_dstr = &get_literal_cstring(temp[i]);
+
+			printf("NAIVE : %u\n", contains_dstring_NAIVE(&str, temp_dstr));
+
+			unsigned int* suffix_prefix_match_length = alloca(sizeof(unsigned int) * (get_char_count_dstring(temp_dstr) + 1));
+			get_prefix_suffix_match_lengths(temp_dstr, suffix_prefix_match_length);
+			printf("KMP   : %u\n", contains_dstring_KMP(&str, temp_dstr, suffix_prefix_match_length));
+
+			printf("RK    : %u\n", contains_dstring_RK(&str, temp_dstr));
+		}
 	}
 
 	printf("\n\nsubstring using std and KMP algorithm\n");
@@ -149,24 +132,33 @@ int main()
 	for(unsigned int i = 0; i <= get_char_count_dstring(slize); i++){printf("%u -> %u\n", i, cache[i]);}
 	printf("\nslize : \"");printf_dstring(slize);printf("\"  O(m*n): %p, O(m+n): %p\n\n", contains_dstring(&str, slize, NULL), contains_dstring(&str, slize, cache));
 
+	printf("str : " printf_dstring_format "\n", printf_dstring_params(&str));
+
 	toLowercase(&str);
-	printf("To lowercase str : ");
-	printf_dstring(&str);
-	printf("\n\n");
+	printf("To lowercase str : \"" printf_dstring_format "\"\n\n", printf_dstring_params(&str));
 
 	toUppercase(&str);
-	printf("To uppercase str : ");
-	printf_dstring(&str);
-	printf("\n\n");
+	printf("To uppercase str : \"" printf_dstring_format "\"\n\n", printf_dstring_params(&str));
 
 	deinit_dstring(&str);
 
-
 	// levenshtein distance formula and length of longest common sub sequence
 	{
-		const dstring* s0 = &get_literal_cstring("TRACE");
-		const dstring* s1 = &get_literal_cstring("CRATE");
-		printf("leven-dist(\"" printf_dstring_format "\", \"" printf_dstring_format "\") = %u\n\n", printf_dstring_params(s0), printf_dstring_params(s1), levenshtein_distance(s0, s1));
-		printf("len-of-lcs(\"" printf_dstring_format "\", \"" printf_dstring_format "\") = %u\n\n", printf_dstring_params(s0), printf_dstring_params(s1), length_of_longest_common_subsequence(s0, s1));
+		char* temp[][2] = {
+			{
+				"TRACE",
+				"CRATE",
+			},
+		};
+
+		int test_count = sizeof(temp) / (sizeof(int) * sizeof(temp[0]));
+
+		for(int i = 0; i < test_count; i++)
+		{
+			const dstring* s0 = &get_literal_cstring(temp[i][0]);
+			const dstring* s1 = &get_literal_cstring(temp[i][1]);
+			printf("leven-dist(\"%s\", \"%s\") = %u\n", temp[i][0], temp[i][1], levenshtein_distance(s0, s1));
+			printf("len-of-lcs(\"%s\", \"%s\") = %u\n\n\n", temp[i][0], temp[i][1], length_of_longest_common_subsequence(s0, s1));
+		}
 	}
 }
