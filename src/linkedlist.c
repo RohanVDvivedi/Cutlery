7ef9618e6ffe11334c09fb26925f047a970e6866
rohan <rohandvivedi@gmail.com>
2021-12-21 16:28:36 +0530

    minor bug resolved

--- src/linkedlist.c
+++ src/linkedlist.c
@@ -272,7 +272,7 @@ int insert_all_after(linkedlist* ll, const void* data_xist, linkedlist* insert_f
 	if(is_new_llnode(ll, node_xist))
 		return 0;
 
-	insert_all_nodes_before(ll, node_xist, insert_from_ll->head);
+	insert_all_nodes_after(ll, node_xist, insert_from_ll->head);
 
 	return 1;
 }
