0b63e4d765de3161dbd071e6a278889de8c03ce6
rohan <rohandvivedi@gmail.com>
2021-12-22 16:55:50 +0530

    updated the arraylist shrink implementation, removed redundant NULL ing code

--- src/arraylist.c
+++ src/arraylist.c
@@ -228,7 +228,7 @@ int shrink_arraylist(arraylist* al)
 {
 	int has_holder_shrunk = 0;
 
-	// to be able to shrink an array, it must have a non-zero size
+	// to be able to shrink an arraylist, it must have a non-zero size
 	// and there is no rotation, i.e. elements are all contiguously placed at index after the first index
 	if((get_capacity_arraylist(al) > 0) && (is_empty_arraylist(al) || (al->first_index + al->element_count) <= get_capacity_arraylist(al)))
 	{
@@ -239,22 +239,12 @@ int shrink_arraylist(arraylist* al)
 						al->arraylist_holder.data_p_p + al->first_index,
 						al->element_count * sizeof(void*));
 
-			// end_index = index that comes after the last index
-			unsigned int elements_to_NULL_from = (al->first_index > al->element_count) ? al->first_index : al->element_count;
-			unsigned int elements_to_NULL_to   = al->first_index + al->element_count - 1;
-			unsigned int elements_to_NULL = elements_to_NULL_to - elements_to_NULL_from + 1;
-			memory_set(al->arraylist_holder.data_p_p + elements_to_NULL_from, 0,
-						elements_to_NULL * sizeof(void*));
-
+			// update first_index of arraylist to 0
 			al->first_index = 0;
 		}
 
 		has_holder_shrunk = shrink_array(&(al->arraylist_holder), al->element_count);
 	}
-
-	// if the arraylist_holder had shrunk, the new first_index has to be at 0ss
-	if(has_holder_shrunk)
-		al->first_index = 0;
 
 	return has_holder_shrunk;
 }
