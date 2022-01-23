e1d1f8c20b43825a36e5c076b9c584ccc1cd4f4e
rohan <rohandvivedi@gmail.com>
2022-01-22 18:29:05 -0700

    implemented singular node functions of singlylist

--- src/singlylist.c
+++ src/singlylist.c
@@ -21,7 +21,7 @@ void initialize_slnode(slnode* node_p)
 
 static int is_new_slnode(const singlylist* sl, const slnode* node_p)
 {
-	return node_p->next == NULL;
+	return node_p->next == NULL && sl->tail != node_p;
 }
 
 int is_empty_singlylist(const singlylist* sl)
@@ -29,22 +29,145 @@ int is_empty_singlylist(const singlylist* sl)
 	return sl->head == NULL;
 }
 
-const void* get_head_of_singlylist(const singlylist* sl);
-const void* get_tail_of_singlylist(const singlylist* sl);
-const void* get_nth_from_head_of_singlylist(const singlylist* sl, unsigned int n);
+const void* get_head_of_singlylist(const singlylist* sl)
+{
+	return (sl->head == NULL) ? NULL : get_data(sl->head);
+}
 
-const void* get_next_of_in_singlylist(const singlylist* sl, const void* data_xist);
+const void* get_tail_of_singlylist(const singlylist* sl)
+{
+	return (sl->tail == NULL) ? NULL : get_data(sl->tail);
+}
 
-int insert_head_in_singlylist(singlylist* sl, const void* data);
-int insert_tail_in_singlylist(singlylist* sl, const void* data);
-int insert_after_in_singlylist(singlylist* sl, const void* data_xist, const void* data);
+const void* get_nth_from_head_of_singlylist(const singlylist* sl, unsigned int n)
+{
+	const slnode* node_p = sl->head;
+	while(node_p != NULL && n > 0)
+	{
+		node_p = node_p->next;
+		n--;
+	}
+	return (node_p == NULL) ? NULL : get_data(node_p);
+}
 
-int insert_all_at_head_in_singlylist(singlylist* sl, singlylist* insert_from_ll);
-int insert_all_at_tail_in_singlylist(singlylist* sl, singlylist* insert_from_ll);
-int insert_all_after_in_singlylist(singlylist* sl, const void* data_xist, singlylist* insert_from_ll);
+const void* get_next_of_in_singlylist(const singlylist* sl, const void* data_xist)
+{
+	const slnode* node_xist = get_node(data_xist);
 
-int remove_head_from_singlylist(singlylist* sl);
-int remove_next_of_from_singlylist(singlylist* sl, const void* data_xist);
+	// the data_xist must not be a new node
+	if(is_new_slnode(sl, node_xist))
+		return NULL;
+	
+	return (node_xist->next == NULL) ? NULL : get_data(node_xist);
+}
+
+int insert_head_in_singlylist(singlylist* sl, const void* data)
+{
+	slnode* node_p = get_node(data);
+
+	// insert only a new node to the singlylist
+	if(!is_new_slnode(sl, data))
+		return 0;
+
+	if(is_empty_singlylist(sl))
+	{
+		sl->head = node_p;
+		sl->tail = node_p;
+	}
+	else
+	{
+		node_p->next = sl->head;
+		sl->head = node_p;
+	}
+
+	return 1;
+}
+
+int insert_tail_in_singlylist(singlylist* sl, const void* data)
+{
+	slnode* node_p = get_node(data);
+
+	// insert only a new node to the singlylist
+	if(!is_new_slnode(sl, data))
+		return 0;
+
+	if(is_empty_singlylist(sl))
+	{
+		sl->head = node_p;
+		sl->tail = node_p;
+	}
+	else
+	{
+		sl->tail->next = node_p;
+		sl->tail = node_p;
+	}
+
+	return 1;
+}
+
+int insert_after_in_singlylist(singlylist* sl, const void* data_xist, const void* data)
+{
+	slnode* node_xist = get_node(data_xist);
+	slnode* node_p = get_node(data);
+
+	// node_xist must not be a new node and node_p must be a new node
+	if(is_new_slnode(sl, node_xist) || !is_new_slnode(sl, node_p))
+		return 0;
+
+	// insert the new node in the singlylist after the node_xist
+	node_p->next = node_xist->next;
+	node_xist->next = node_p;
+
+	// if node_xist was the tail node then, now node_p is the tail node
+	if(sl->tail == node_xist)
+		sl->tail = node_p;
+
+	return 1;
+}
+
+int insert_all_at_head_in_singlylist(singlylist* sl, singlylist* insert_from_sl);
+int insert_all_at_tail_in_singlylist(singlylist* sl, singlylist* insert_from_sl);
+int insert_all_after_in_singlylist(singlylist* sl, const void* data_xist, singlylist* insert_from_sl);
+
+int remove_head_from_singlylist(singlylist* sl)
+{
+	if(is_empty_singlylist(sl))
+		return 0;
+
+	slnode* to_be_removed = sl->head;
+
+	sl->head = to_be_removed->next;
+	if(sl->head == NULL)
+		sl->tail = NULL;
+
+	initialize_slnode(to_be_removed);
+
+	return 1;
+}
+
+int remove_next_of_from_singlylist(singlylist* sl, const void* data_xist)
+{
+	if(is_empty_singlylist(sl))
+		return 0;
+
+	slnode* node_xist = get_node(data_xist);
+
+	// node_xist must not be a new node
+	if(is_new_slnode(sl, node_xist))
+		return 0;
+
+	slnode* to_be_removed = node_xist->next;
+
+	node_xist->next = to_be_removed->next;
+
+	// if the node to be removed was a tail node then update the tail node to the node_xist
+	if(sl->tail == to_be_removed)
+		sl->tail = node_xist;
+
+	initialize_slnode(to_be_removed);
+
+	return 1;
+}
 
 void remove_all_from_singlylist(singlylist* sl);
 
