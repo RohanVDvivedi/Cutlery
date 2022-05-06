453055b8ddfbdaafb76c81b1557238d814ba41b7
RohanVDvivedi <rohandvivedi@gmal.com>
2022-05-06 16:21:53 -0700

    added comments to the use of get_node and get_data macros

--- inc/cutlery_stds.h
+++ inc/cutlery_stds.h
@@ -23,10 +23,12 @@
 
 /*
 *	MACROS TO CONVERT FROM DATA TO NODE AND NODE TO DATA
-*  This is only used by embedded node based data structures
+*  This is only used by embedded node based data structures.
+*  All embedded node based data structures (like linkedlist, singlylist, bst and heap)
+*  store node_offset to their nodes.
+*  Since, their nodes (that these datastructures operate on) are always at this fixed offset inside the user's data (structs).
 */
 #define get_data(node_p, ds_p) 	(((const void*)(node_p)) - ((ds_p)->node_offset))
-
 #define get_node(data_p, ds_p) 	(((void*)(data_p)) + ((ds_p)->node_offset))
 
 /* this is cutlery specific memory move implementation
