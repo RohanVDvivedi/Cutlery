53fa5ad1baaf44530b5adbfbd6721fcea50672b4
RohanVDvivedi <rohandvivedi@gmail.com>
2022-08-21 15:24:01 -0700

    bst_nonbalancing finished revamp node_property

--- src/bst_nonbalancing.c
+++ src/bst_nonbalancing.c
@@ -7,9 +7,15 @@
 void insert_node_in_non_self_balancing_tree(bst* bst_p, bstnode* node_p)
 {
 	insert_node_in_bst(bst_p, node_p);
+
+	// the new node now exists in bst, so set its node_property to 1
+	node_p->node_property = 1;
 }
 
 void remove_node_from_non_self_balancing_tree(bst* bst_p, bstnode* node_p)
 {
 	remove_node_from_bst(bst_p, node_p);
+
+	// the node now does not exists in bst, so set its node_property to 0
+	node_p->node_property = 0;
 }
