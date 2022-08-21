14837297542a21c5eaccf38c878b818579e8b8b7
RohanVDvivedi <rohandvivedi@gmail.com>
2022-08-21 14:59:11 -0700

    added necesary comments

--- inc/bst_core.h
+++ inc/bst_core.h
@@ -7,8 +7,13 @@ bstnode* get_smallest_node_from_node(bstnode* node_p);
 
 bstnode* get_largest_node_from_node(bstnode* node_p);
 
+// inserts a node in the bst
+// the node must be a new_node -> passing is_new_bstnode as true
 void insert_node_in_bst(bst* bst_p, bstnode* node_p);
 
+// the node must exist in the same bst
+// the node that gets removed will still hold its parent, left and right pointers and its node property
+// the removed node node must be reinitialized after this call using -> initialize_bstnode function
 void remove_node_from_bst(bst* bst_p, bstnode* node_p);
 
 #endif
