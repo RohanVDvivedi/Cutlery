f2fc38b2cc8ff702a17e3dc07a75ad02aa8f6051
RohanVDvivedi <rohandvivedi@gmail.com>
2022-08-21 14:33:27 -0700

    added cutlery math header

---
+++ inc/cutlery_math.h
@@ -0,0 +1,8 @@
+#ifndef CUTLERY_MATH_H
+#define CUTLERY_MATH_H
+
+#define min(a,b) (((a)<(b))?(a):(b))
+#define max(a,b) (((a)>(b))?(a):(b))
+#define compare(a,b) (((a)<(b))?(-1):(((a)>(b))?(1):(0)))
+
+#endif
