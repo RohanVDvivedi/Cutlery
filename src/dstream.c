13537a94ba4c8e2f369c8a9e3afdbee8f6318a02
RohanVDvivedi <rohandvivedi@gmail.com>
2022-08-30 13:55:01 -0700

    implemented dstream initialize and deinitialize functons

--- src/dstream.c
+++ src/dstream.c
@@ -3,9 +3,32 @@
 #include<cutlery_stds.h>
 #include<circular_buffer_array_util.h>
 
-void initialize_dstream(dstream* strm, unsigned int capacity);
-void initialize_dstream_with_allocator(dstream* strm, unsigned int capacity, memory_allocator buffer_allocator);
-void initialize_dstream_with_memory(dstream* strm, unsigned int capacity, void* buffer);
+void initialize_dstream(dstream* strm, unsigned int capacity)
+{
+	initialize_dstream_with_allocator(strm, capacity, STD_C_mem_allocator);
+}
+
+void initialize_dstream_with_allocator(dstream* strm, unsigned int capacity, memory_allocator buffer_allocator)
+{
+	strm->read_closed = 0;
+	strm->write_closed = 0;
+	strm->buffer_capacity = capacity;
+	strm->first_byte = 0;
+	strm->byte_count = 0;
+	strm->buffer_allocator = buffer_allocator;
+	strm->buffer = allocate(strm->buffer_allocator, &(strm->buffer_capacity));
+}
+
+void initialize_dstream_with_memory(dstream* strm, unsigned int capacity, void* buffer)
+{
+	strm->read_closed = 0;
+	strm->write_closed = 0;
+	strm->buffer_capacity = capacity;
+	strm->first_byte = 0;
+	strm->byte_count = 0;
+	strm->buffer_allocator = NULL;
+	strm->buffer = buffer;
+}
 
 unsigned int write_to_dstream(dstream* strm, const void* data, unsigned int data_size, dstream_operation_type op_type)
 {
@@ -34,7 +57,11 @@ unsigned int push_back_to_dstream(dstream* strm, const void* data, unsigned int 
 unsigned int pop_front_from_dstream(dstream* strm, void* data, unsigned int data_size, dstream_operation_type op_type);
 unsigned int pop_back_from_dstream(dstream* strm, void* data, unsigned int data_size, dstream_operation_type op_type);
 
-void remove_all_from_dstream(dstream* strm);
+void remove_all_from_dstream(dstream* strm)
+{
+	strm->first_byte = 0;
+	strm->byte_count = 0;
+}
 
 int is_empty_dstream(const dstream* strm)
 {
@@ -62,4 +89,14 @@ int is_closed_for_reader(const dstream* strm)
 	return strm->read_closed;
 }
 
-void deinitialize_dstream();
+void deinitialize_dstream()
+{
+	is_closed_for_writer(strm);
+	is_closed_for_reader(strm);
+	remove_all_from_dstream(strm);
+	if(strm->buffer_capacity > 0 && strm->buffer != NULL && strm->buffer_allocator != NULL)
+		deallocate(strm->buffer_allocator, strm->buffer, strm->buffer_capacity);
+	strm->buffer_allocator = NULL;
+	strm->buffer_capacity = 0;
+	strm->buffer = NULL;
+}
