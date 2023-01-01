249a8ae0aa0dd39728a91a8675a4167931a00d5a
RohanVDvivedi <rohandvivedi@gmail.com>
2023-01-01 15:59:23 -0700

    added comment for usage of i for getiinng children of N-ary tree as array util

--- inc/nary_tree_as_array_util.h
+++ inc/nary_tree_as_array_util.h
@@ -26,6 +26,7 @@
 #define has_parent(index)								(! is_root_index((index)))
 #define get_parent_index_N(child_index, N) 				(((child_index) - 1) / N)
 
+// here i must be in range [0, N) (0 inclusive, N exclusive)
 #define get_index_of_ith_child_N(parent_index, i, N)		((N * (parent_index)) + 1 + i)
 
 #endif
