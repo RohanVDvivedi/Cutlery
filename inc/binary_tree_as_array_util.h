cb1f746cc8ca36b6cc3e70a35ab4ff03fbd266f3
RohanVDvivedi <rohandvivedi@gmail.com>
2023-01-01 15:56:48 -0700

    added nary tree as array util

--- inc/binary_tree_as_array_util.h
+++ inc/binary_tree_as_array_util.h
@@ -3,16 +3,45 @@
 
 #include<cutlery_stds.h>
 
-#define is_root_index(index)					((index) == 0)
+#include<nary_tree_as_array_util.h>
 
-// a root in a binary tree will not have any parent
-#define has_parent(index)						(! is_root_index((index)))
-#define get_parent_index(child_index) 			(((child_index) - 1) / 2)
+/*
+	binary tree as array is a special case of N-ary tree as an array
+	here each node has atmost 2 children
+	and just like N-ary tree as array, binary tree as array is also completely filled
+	refer to "nary_tree_as_array_util.h"
+
+	standard binary tree as array can be represented as follows
+
+	all indexes are unsigned ints
+	array form (when indexed) :
+
+	0 1 2 3 4 5 6 7 8 9 10 11 ...
+
+	tree form (index to tree node position mapping) :
+	             0
+	           /   \
+	         /       \
+	        1         2
+	       /  \     /   \
+	      3   4     5    6
+	     /\   /\   / \   /\
+	    7 8  9 10 11 12 13 14
+	   /
+	  15
+
+	as you can see in a binary tree, the first element of each of the row is always the number that is of the form 0000...000111...11
+	i.e. all 0s and then all 1s
+	thus for a 32 bit number (most unsigned int), the minimum index that does not have children is (UINT_MAX >> 1)
+	Note :: as per cutlery standards UINT_MAX is an unindexable number
+*/
+
+#define get_parent_index(child_index) 			get_parent_index(child_index, 2)
 
 // if the index is more than or equal to (UINT_MAX / 2), then calculating its child index is always an overflow
 #define can_have_any_children(index)			((index) < ((UINT_MAX) >> 1))
-#define get_left_child_index(parent_index)		((2 * (parent_index)) + 1)
-#define get_right_child_index(parent_index)		((2 * (parent_index)) + 2)
+#define get_left_child_index(parent_index)		get_index_of_ith_child_N(parent_index, 0, 2)
+#define get_right_child_index(parent_index)		get_index_of_ith_child_N(parent_index, 1, 2)
 
 #define get_sibling_index(sibling_index)		((((sibling_index) + 1) ^ 1) - 1)
 
---
+++ inc/nary_tree_as_array_util.h
@@ -0,0 +1,31 @@
+#ifndef NARY_TREE_ARRAY_UTIL_H
+#define NARY_TREE_ARRAY_UTIL_H
+
+/*
+	standard N-ary tree as array, (with N=3) can be represented as follows
+
+	all indexes are unsigned ints
+	array form (when indexed) :
+
+	0 1 2 3 4 5 6 7 8 9 10 11 ...
+
+	tree form (index to tree node position mapping) :
+	               0
+	           /  |    \
+	         /    |     \
+	        1     2      3
+	       /|\   /|\   /  | \
+	      3 4 5 6 7 8  9 10 11
+	     /| 
+	   12 13
+*/
+
+#define is_root_index(index)							((index) == 0)
+
+// a root in the N-ary tree will not have any parent
+#define has_parent(index)								(! is_root_index((index)))
+#define get_parent_index_N(child_index, N) 				(((child_index) - 1) / N)
+
+#define get_index_of_ith_child_N(parent_index, i, N)		((N * (parent_index)) + 1 + i)
+
+#endif
