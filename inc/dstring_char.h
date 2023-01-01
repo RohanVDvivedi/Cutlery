a99ef78adfd0693d285733c8a0200e544e77ef2d
RohanVDvivedi <rohandvivedi@gmail.com>
2023-01-01 12:39:07 -0700

    added dstring char method declarations

---
+++ inc/dstring_char.h
@@ -0,0 +1,12 @@
+#ifndef DSTRING_CHAR_H
+#define DSTRING_CHAR_H
+
+int is_digit_char(char c);
+int is_whitespace_char(char c);
+int is_lowercase_char(char c);
+int is_uppercase_char(char c);
+
+char to_lowercase_char(char c);
+char to_uppercase_char(char c);
+
+#endif
