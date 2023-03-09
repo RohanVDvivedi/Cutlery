ba02ef23376be31c6ce6c1433574ea2347e22f1f
RohanVDvivedi <rohandvivedi@gmail.com>
2023-03-08 22:57:39 -0700

    added logic to discard new lines from all shell calls (echo and base64)

--- test/test.sh
+++ test/test.sh
@@ -3,8 +3,10 @@ rm ./a.out
 if [ $1 = "testBase64" ]
 then
 	gcc $1.c -lcutlery
-	echo "Rohan Vipulkumar Dvivedi" | ./a.out | base64 --decode
-	echo "Rohan Vipulkumar Dvivedi" | base64 | ./a.out --decode
+	echo -n "Rohan Vipulkumar Dvivedi" | ./a.out | base64 --decode -w 0
+	echo ""
+	echo -n "Rohan Vipulkumar Dvivedi" | base64 -w 0 | ./a.out --decode
+	echo ""
 else
 	gcc $1.c -lcutlery && sudo valgrind -v ./a.out
 fi
