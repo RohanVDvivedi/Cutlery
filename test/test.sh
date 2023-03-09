0b883b58fd1ab07632ffaf39d209180e5013cfed
RohanVDvivedi <rohandvivedi@gmail.com>
2023-03-08 22:38:55 -0700

    added test cases to test base 64

--- test/test.sh
+++ test/test.sh
@@ -1,1 +1,10 @@
-gcc $1.c -lcutlery && sudo valgrind -v ./a.out
+rm ./a.out
+
+if [ $1 = "testBase64" ]
+then
+	gcc $1.c -lcutlery
+	echo "Rohan Vipulkumar Dvivedi" | ./a.out | base64 --decode
+	echo "Rohan Vipulkumar Dvivedi" | base64 | ./a.out --decode
+else
+	gcc $1.c -lcutlery && sudo valgrind -v ./a.out
+fi
---
+++ test/testBase64.c
@@ -0,0 +1,76 @@
+#include<unistd.h>
+#include<stdio.h>
+#include<string.h>
+
+#include<dstring_base64.h>
+
+dstring read_all_from_stdin()
+{
+	#define BUFFER_SIZE 4096
+	dstring res;
+	init_empty_dstring(&res, 0);
+	while(1)
+	{
+		char buffer[BUFFER_SIZE];
+		unsigned int buffer_size = read(0, buffer, BUFFER_SIZE);
+		if(buffer_size == 0)
+			break;
+		concatenate_dstring(&res, &get_dstring_pointing_to(buffer, buffer_size));
+	}
+	return res;
+}
+
+int read_from_stdin_and_encode()
+{
+	dstring in = read_all_from_stdin();
+
+	dstring res;
+	init_empty_dstring(&res, 0);
+
+	int return_val = 0;
+
+	if(base64_encode(&in, &res) == 0)
+	{
+		printf("not encodable\n");
+		return_val = -1;
+	}
+	else
+		printf_dstring(&res);
+
+	deinit_dstring(&res);
+	deinit_dstring(&in);
+	return return_val;
+}
+
+int read_from_stdin_and_decode()
+{
+	dstring in = read_all_from_stdin();
+
+	dstring res;
+	init_empty_dstring(&res, 0);
+
+	int return_val = 0;
+
+	if(base64_decode(&in, &res) == 0)
+	{
+		printf("not decodable\n");
+		return_val = -1;
+	}
+	else
+		printf_dstring(&res);
+
+	deinit_dstring(&res);
+	deinit_dstring(&in);
+	return return_val;
+}
+
+int main(int argc, char** argv)
+{
+	if(argc == 1)
+		return read_from_stdin_and_encode();
+	else if(argc == 2 && 0 == strcmp("--decode", argv[1]))
+		return read_from_stdin_and_decode();
+
+	printf("usage error you can either do :\n `cat file | base64`\n or :\n `cat file | base64 --decode`\n");
+	return -1;
+}
