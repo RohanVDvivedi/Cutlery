193eb1ee70455fe6161a0ed12d4f128d64f236c8
RohanVDvivedi <rohandvivedi@gmail.com>
2023-03-08 19:41:55 -0700

    implemented is_valid_base64_encoded function

--- src/base64.c
+++ src/base64.c
@@ -11,6 +11,14 @@
 // maximum length of the dstring that we can encode
 #define MAX_ENCODABLE_SIZE (MAX_PACKET_COUNT * 3)
 
+static int is_valid_base64_encoded_char(char c)
+{
+	// i.e. A-Z, a-z, 0-9, + and /
+	if(('A' <= c && c <= 'Z') || ('a' <= c && c <= 'z') || ('0' <= c && c <= '9') || (c == '+') || (c == '/'))
+		return 1;
+	return 0;
+}
+
 int is_valid_base64_encoding(const dstring* base64_enc)
 {
 	unsigned int base64_enc_size = get_char_count_dstring(base64_enc);
@@ -21,7 +29,32 @@ int is_valid_base64_encoding(const dstring* base64_enc)
 	const char* base64_enc_data = get_byte_array_dstring(base64_enc);
 
 	// check if all chars are valid i.e. A-Z, a-z, 0-9, + and / until an = is encountered
-	// TODO
+	unsigned int i = 0;
+	while(i < base64_enc_size)
+	{
+		if(!is_valid_base64_encoded_char(base64_enc_data[i]))
+		{
+			// '=' implies we are in the last packet
+			if(base64_enc_data[i] == '=')
+				break;
+			else
+				return 0;
+		}
+	}
+
+	// we hit '=', or end of base64_enc
+	// there can be 0, 1, or 2 '=' at the end
+	if((base64_enc_size - i) > 2)
+		return 0;
+
+	// check that all the remaining ones are '=' indeed
+	while(i < base64_enc_size)
+	{
+		if(base64_enc_data[i] != '=')
+			return 0;
+	}
+
+	return 1;
 }
 
 int can_base64_decode(const dstring* base64_enc)
