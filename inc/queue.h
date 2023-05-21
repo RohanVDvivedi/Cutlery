af283ccd534c7a03362d4e8f6e0d565cf0790214
RohanVDvivedi <rohandvivedi@gmail.com>
2023-05-21 05:00:55 -0700

    startd replacing unsigned int with cy_uint in queue.h header

--- inc/queue.h
+++ inc/queue.h
@@ -13,9 +13,9 @@ struct queue
 // initializes queue and it will depend on initialize_array to give necessary memory to manage internal element contents
 // the initialize_queue* functions may fail if the initial memory allocation fails
 // in case of a failure, the queue will still be initialized to capacity 0
-int initialize_queue(queue* queue_p, unsigned int capacity);
-int initialize_queue_with_allocator(queue* queue_p, unsigned int capacity, memory_allocator mem_allocator);
-void initialize_queue_with_memory(queue* queue_p, unsigned int capacity, const void* data_ps[]);
+int initialize_queue(queue* queue_p, cy_uint capacity);
+int initialize_queue_with_allocator(queue* queue_p, cy_uint capacity, memory_allocator mem_allocator);
+int initialize_queue_with_memory(queue* queue_p, cy_uint capacity, const void* data_ps[]);
 
 // push a new element to the queue
 // push returns 1, if data_p is successfully pushed
@@ -30,7 +30,7 @@ int pop_from_queue(queue* queue_p);
 
 // returns pointer to the top element data pointer, (i.e. data pointer to the earliest inserted element) 
 const void* get_top_of_queue(const queue* queue_p);
-const void* get_nth_from_top_of_queue(const queue* queue_p, unsigned int n);
+const void* get_nth_from_top_of_queue(const queue* queue_p, cy_uint n);
 
 // removes all the elements from queue queue_p
 // after a call to this function the queue contains 0 elements (get_element_count() == 0)
@@ -42,10 +42,10 @@ void remove_all_from_queue(queue* queue_p);
 void deinitialize_queue(queue* queue_p);
 
 // returns capacity of the queue, i.e. the maximum number of elements that can be accomodated in queue
-unsigned int get_capacity_queue(const queue* queue_p);
+cy_uint get_capacity_queue(const queue* queue_p);
 
 // returns the number of elements inside the queue
-unsigned int get_element_count_queue(const queue* queue_p);
+cy_uint get_element_count_queue(const queue* queue_p);
 
 // returns 1, if the queue container is full, i.e. it needs to be expanded to push any futher elements
 // else it return 0, this means there is still space in the container to push elements
@@ -66,10 +66,10 @@ int shrink_queue(queue* queue_p);
 
 // expands queue to atleast the capacity of atleast_capacity
 // returns 1, only if the queue was expanded
-int reserve_capacity_for_queue(queue* queue_p, unsigned int atleast_capacity);
+int reserve_capacity_for_queue(queue* queue_p, cy_uint atleast_capacity);
 
 // for each function, to iteration over all non null elements to perform an operation
-void for_each_in_queue(const queue* queue_p, void (*operation)(void* data_p, unsigned int index, const void* additional_params), const void* additional_params);
+void for_each_in_queue(const queue* queue_p, void (*operation)(void* data_p, cy_uint index, const void* additional_params), const void* additional_params);
 
 // serializes the queue, and appends the serialized form to the dstring
 void sprint_queue(dstring* append_str, const queue* queue_p, void (*sprint_element)(dstring* append_str, const void* data_p, unsigned int tabs), unsigned int tabs);
