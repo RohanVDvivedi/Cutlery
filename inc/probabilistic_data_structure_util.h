d53e8400037c67bb57327ace98e0d2f2aeb7f9b0
RohanVDvivedi <rohandvivedi@gmail.com>
2023-05-22 23:11:01 -0700

    modified hash function typedef declaration for probablistic data structures

--- inc/probabilistic_data_structure_util.h
+++ inc/probabilistic_data_structure_util.h
@@ -2,6 +2,6 @@
 #define PROBABLISTIC_DATA_STRUCTURE_UTIL_H
 
 // the hash function definition
-typedef unsigned int (*data_hash_func)(const void* data, unsigned int data_size);
+typedef cy_uint (*data_hash_func)(const void* data, cy_uint data_size);
 
 #endif
