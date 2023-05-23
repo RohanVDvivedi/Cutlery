1133c2c23fa7f5a13b62f4c4e8c392d2657ba40b
RohanVDvivedi <rohandvivedi@gmail.com>
2023-05-22 23:32:13 -0700

    modified multi dim array util to use cy_uint instead of unsigned int

--- inc/multi_dim_array_util.h
+++ inc/multi_dim_array_util.h
@@ -41,22 +41,24 @@
 **	Notice the reverse order in supplying the dimensions and indices array ???
 */
 
+// It is your responsibility to ensure that the product of all the dimensions does not overflow cy_uint
+
 // total number of elements in multi dimension array
-unsigned int get_element_count_in_multi_dim_array(const unsigned int* dims, unsigned int dims_count);
+cy_uint get_element_count_in_multi_dim_array(const cy_uint* dims, cy_uint dims_count);
 
 // accessor to array[index[dims_count-1]]...[index[1]][index[0]]
 // is given by array[accessor]
 // please make sure that all the indices are in range
 // i.e. for any i form 0 to dims_count - 1 both inclusive ==>>> 0 <= index[i] < dims[i]
-unsigned int get_accessor_from_indices(const unsigned int* indices, const unsigned int* dims, unsigned int dims_count);
+cy_uint get_accessor_from_indices(const cy_uint* indices, const cy_uint* dims, cy_uint dims_count);
 
 // returns maximum index that is possible for a dimension at dim_no for given dimensions list
-unsigned int get_max_index_in_multi_dim_array(unsigned int dim_no, const unsigned int* dims, unsigned int dims_count);
+cy_uint get_max_index_in_multi_dim_array(cy_uint dim_no, const cy_uint* dims, cy_uint dims_count);
 
 // accessor to array[index[dims_count-1]]...[index[1]][index[0]]
 // is given by array[accessor]
 // please make sure that the accessor in in range
 // i.e. 0 <= accessor < ( dims[dims_count - 1] * ... * dims[1] * dims[0] )
-void get_indices_from_accessor(unsigned int* indices, unsigned int accessor, const unsigned int* dims, unsigned int dims_count);
+void get_indices_from_accessor(cy_uint* indices, cy_uint accessor, const cy_uint* dims, cy_uint dims_count);
 
 #endif
--- src/multi_dim_array_util.c
+++ src/multi_dim_array_util.c
@@ -1,23 +1,23 @@
 #include<multi_dim_array_util.h>
 
-unsigned int get_element_count_in_multi_dim_array(const unsigned int* dims, unsigned int dims_count)
+cy_uint get_element_count_in_multi_dim_array(const cy_uint* dims, cy_uint dims_count)
 {
-	unsigned int element_count = 0;
-	for(unsigned int dim = 0; dim < dims_count; dim++)
+	cy_uint element_count = 0;
+	for(cy_uint dim = 0; dim < dims_count; dim++)
 		element_count *= dims[dim];
 	return element_count;
 }
 
-unsigned int get_accessor_from_indices(const unsigned int* indices, const unsigned int* dims, unsigned int dims_count)
+cy_uint get_accessor_from_indices(const cy_uint* indices, const cy_uint* dims, cy_uint dims_count)
 {
-	unsigned int accessor = 0;
-	unsigned int elements_under_ith_dimension = 1;
-	// elements_under_the_ith_dimension is the number of elements that you jump when ith index (0 <= indices[i] < dims[i]) changes by 1
+	cy_uint accessor = 0;
+	cy_uint elements_under_ith_dimension = 1;
+	// elements_under_the_ith_dimension is the number of elements that you jump when ith dimension's index (0 <= indices[i] < dims[i]) changes by 1
 	// 0 -> 1
 	// 1 -> 1 * dims[0]
 	// 2 -> 1 * dims[0] * dims[1]
 	// 3 -> ... and so on,
-	for(unsigned int dim = 0; dim < dims_count; dim++)
+	for(cy_uint dim = 0; dim < dims_count; dim++)
 	{
 		accessor += (elements_under_ith_dimension * indices[dim]);
 		elements_under_ith_dimension *= dims[dim];
@@ -25,20 +25,20 @@ unsigned int get_accessor_from_indices(const unsigned int* indices, const unsign
 	return accessor;
 }
 
-unsigned int get_max_index_in_multi_dim_array(unsigned int dim_no, const unsigned int* dims, unsigned int dims_count)
+cy_uint get_max_index_in_multi_dim_array(cy_uint dim_no, const cy_uint* dims, cy_uint dims_count)
 {
 	return dims[dim_no] - 1;
 }
 
-void get_indices_from_accessor(unsigned int* indices, unsigned int accessor, const unsigned int* dims, unsigned int dims_count)
+void get_indices_from_accessor(cy_uint* indices, cy_uint accessor, const cy_uint* dims, cy_uint dims_count)
 {
-	unsigned int elements_under_ith_dimension = 1;
+	cy_uint elements_under_ith_dimension = 1;
 	// elements_under_the_ith_dimension is the number of elements that you jump when ith index (0 <= indices[i] < dims[i]) changes by 1
 	// 0 -> 1
 	// 1 -> 1 * dims[0]
 	// 2 -> 1 * dims[0] * dims[1]
 	// 3 -> ... and so on,
-	for(unsigned int dim = 0; dim < dims_count; dim++)
+	for(cy_uint dim = 0; dim < dims_count; dim++)
 	{
 		indices[dim] = (accessor / elements_under_ith_dimension) % dims[dim];
 		elements_under_ith_dimension *= dims[dim];
