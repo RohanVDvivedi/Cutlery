b63db0475890c1a291ec59e3009d4d64e3177135
RohanVDvivedi <rohandvivedi@gmail.com>
2023-05-26 21:29:13 -0700

    modified dstring.h to use cy_uint instead of unsiged int

--- inc/dstring.h
+++ inc/dstring.h
@@ -18,10 +18,10 @@ struct dstring
 	unsigned char type_n_SS_size;
 
 	// number of bytes in the byte_array
-	unsigned int bytes_occupied;
+	cy_uint bytes_occupied;
 
 	// maxumum number of bytes at byte_array
-	unsigned int bytes_allocated;
+	cy_uint bytes_allocated;
 
 	// non null terminating string
 	char* byte_array;
@@ -32,7 +32,7 @@ struct dstring
 
 dstring_type get_dstring_type(const dstring* str_p);
 
-void init_empty_dstring(dstring* str_p, unsigned int capacity);
+void init_empty_dstring(dstring* str_p, cy_uint capacity);
 
 #define get_dstring_pointing_to(data, data_size) \
 	((const dstring){.type_n_SS_size = POINT_DSTR, .byte_array = ((char*)(data)), .bytes_occupied = (data_size), .bytes_allocated = 0})
@@ -43,25 +43,25 @@ void init_empty_dstring(dstring* str_p, unsigned int capacity);
 void deinit_dstring(dstring* str_p);
 
 char* get_byte_array_dstring(const dstring* str_p);
-unsigned int get_char_count_dstring(const dstring* str_p);
-int increment_char_count_dstring(dstring* str_p, unsigned int increment_by);
-int decrement_char_count_dstring(dstring* str_p, unsigned int decrement_by);
-unsigned int get_capacity_dstring(const dstring* str_p);
-unsigned int get_unused_capacity_dstring(const dstring* str_p);
+cy_uint get_char_count_dstring(const dstring* str_p);
+int increment_char_count_dstring(dstring* str_p, cy_uint increment_by);
+int decrement_char_count_dstring(dstring* str_p, cy_uint decrement_by);
+cy_uint get_capacity_dstring(const dstring* str_p);
+cy_uint get_unused_capacity_dstring(const dstring* str_p);
 
 void make_dstring_empty(dstring* str_p);
 
 // increases the size of dstring by additional_size number of bytes
-int expand_dstring(dstring* str_p, unsigned int additional_size);
+int expand_dstring(dstring* str_p, cy_uint additional_size);
 // shrinks dstring to its bytes_occupied size
 int shrink_dstring(dstring* str_p);
 
 // BASE METHODS END
 
-void init_dstring(dstring* str_p, const char* data, unsigned int data_size);
+void init_dstring(dstring* str_p, const char* data, cy_uint data_size);
 void init_copy_dstring(dstring* str_p, const dstring* init_copy_from);
 
-dstring new_dstring(const char* data, unsigned int data_size);
+dstring new_dstring(const char* data, cy_uint data_size);
 dstring new_copy_dstring(const dstring* init_copy_from);
 
 // checks if the dstring is empty, returns 1 if it is empty
@@ -75,11 +75,11 @@ int case_compare_dstring(const dstring* str_p1, const dstring* str_p2);
 // discard chars in dstring between start_index and last_index both inclusive
 // it won't shrink the dstring
 // this function will fail for POINT_DSTR
-int discard_chars_dstring(dstring* str_p, unsigned int start_index, unsigned int last_index);
+int discard_chars_dstring(dstring* str_p, cy_uint start_index, cy_uint last_index);
 
 // this function can be called on all types of dstrings
-int discard_chars_from_front_dstring(dstring* str_p, unsigned int bytes_to_discard);
-int discard_chars_from_back_dstring(dstring* str_p, unsigned int bytes_to_discard);
+int discard_chars_from_front_dstring(dstring* str_p, cy_uint bytes_to_discard);
+int discard_chars_from_back_dstring(dstring* str_p, cy_uint bytes_to_discard);
 
 // concatenates str_p2 to str_p1, this functions fail only if memory allocation fails (a worrysome situation)
 int concatenate_dstring(dstring* str_p1, const dstring* str_p2);
@@ -87,7 +87,7 @@ int concatenate_c_string(dstring* str_p1, const char* c_string);
 int concatenate_char(dstring* str_p1, char chr);
 
 // append count number of chr char at the end of the dstring str_p
-void sprint_chars(dstring* str_p, char chr, unsigned int count);
+void sprint_chars(dstring* str_p, char chr, cy_uint count);
 
 // marcos for io using dstring
 #define STRINGIFY2(X) #X
@@ -103,14 +103,14 @@ void to_uppercase_dstring(dstring* str_p);
 
 // trim white spaces from dstring, returns the number of white spaces removed
 // they won't shrink the dstring
-unsigned int ltrim_dstring(dstring* str_p);
-unsigned int rtrim_dstring(dstring* str_p);
-unsigned int trim_dstring(dstring* str_p);
+cy_uint ltrim_dstring(dstring* str_p);
+cy_uint rtrim_dstring(dstring* str_p);
+cy_uint trim_dstring(dstring* str_p);
 
 // returns 1 in successfull conversion
 // it will return 0 if a character other than a radix character is found
 // this function does not check for overflow, hence it must be you who takes care of that, by checking the length of the string provided
-int get_unsigned_int_from_dstring(const dstring* str_p, unsigned int radix, unsigned int* result);
+int get_unsigned_long_long_int_from_dstring(const dstring* str_p, unsigned int radix, unsigned long long int* result);
 
 #include<dstring_substring.h>
 #include<dstring_diff.h>
