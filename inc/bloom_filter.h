1b96756135a80642886aeec9e034727ff408dc55
RohanVDvivedi <rohandvivedi@gmail.com>
2023-06-01 09:06:09 -0700

    minor change in comments and made bucket_count -> capacity in bytes

--- inc/bloom_filter.h
+++ inc/bloom_filter.h
@@ -8,9 +8,9 @@
 /*
 ** Bloom Filter is a probablistic data structure
 ** It intends to provide you with a check to see if an object/data has been seen by you or not
-** Since it is a probalistic data structure if may return false positives, But there are no false negatives
-** higher the total number of buckets (number of hash functions * bucket_count) higher is its accuracy
-** (with ofcourse better hash_functions with distributed and uniform over the range)
+** Since it is a probalistic data structure it may return false positives, but there are no false negatives
+** higher the total number of buckets (number of hash functions * bucket_count per hash function) higher is its accuracy
+** (with ofcourse better hash_functions with wide distribution and uniform over the range)
 **
 ** you must call "insert_in_bloom_filter" to mark a data as seen
 ** while you can call "exists_in_bloom_filter" any time to check if a piece of data was seen earlier. (i.e. was marked earlier as seen)
@@ -22,11 +22,12 @@ struct bloom_filter
 	// array consisting of all hash functions
 	array data_hash_functions;
 
-	// each data_hash_function has bucket_count number of buckets
-	// total number of buckets = number of hash functions * bucket_count
-	cy_uint bucket_count;
+	// there are capacity_in_bytes number of bytes at address pointed by bitmap
+	// each data_hash_function has ((capacity_in_bytes * CHAR_BIT) / get_capacity_array(data_hash_functions)) number of buckets
+	// total number of buckets = get_capacity_array(data_hash_functions) * ((capacity_in_bytes * CHAR_BIT) / get_capacity_array(data_hash_functions))
+	cy_uint capacity_in_bytes;
 
-	// This is a bitmap consisting of (total bucket count) number of bits
+	// This is a bitmap consisting total of (capacity_in_bytes * CHAR_BIT) number of bits
 	// this bitmap is a single chunk of memory
 	// if the bitmap was managed as array by the c language, then it would be accessible as bitmap[ ith_hash_function ][ jth_bit ]
 	// here we will use the functionality defined in bitmap.h and multi_dim_array_util.h to create a packed (single allocation) multi dimensional bitmap
@@ -59,7 +60,7 @@ enum bloom_filter_presence
 
 // a result of exists in bloom_filter is 
 // a definite not present (i.e. not present with 100% surety)
-// or it may be present (it may be present or absent)
+// or it may be present (it could be present or absent)
 bloom_filter_presence exists_in_bloom_filter(const bloom_filter* bf_p, const void* data, cy_uint data_size);
 
 // all bits will be reset i.e. all the elements will be made ABSENT
