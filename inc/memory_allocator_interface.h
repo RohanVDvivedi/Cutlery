40e46cc12ca60396813e121648ee9dc0f80713c7
RohanVDvivedi <rohandvivedi@gmail.com>
2023-06-01 05:50:34 -0700

    minor change to comments adding more functionality to the memory allocator interface

--- inc/memory_allocator_interface.h
+++ inc/memory_allocator_interface.h
@@ -33,7 +33,7 @@ enum memory_allocator_initialization
 ** allocator_context : context to this memory allocator
 ** old_memory        : memory allocated in the past, that you no longer require, this memory must/will be freed
 ** old_size          : size of memory pointed to by old_allocation (your custom allocator may or may not use this value)
-** new_size          : size of new memory that you want (this is a pointer hence the actual allocated size is returned)
+** new_size          : size of new memory that you want (this is a pointer hence the actual allocated size is returned) (a NULL or (pointer to) 0 in this parameter implies - only deallocate old memory)
 ** new_alignment     : the alignment required for the new memory (0 or 1 represents no memory alignment requirements)
 ** initialization    : check enum memory_allocator_initialization
 **
@@ -43,14 +43,20 @@ enum memory_allocator_initialization
 **		it must return a pointer that is multiple of "new_alignment", and can safely hold atleast "new_size" number of bytes
 **		and these "new_size" number of bytes must be initialized according to "initialization" enum memory_allocator_initialization parameter
 **
-** 		The returned memory pointer must point to atleast new_size number of bytes
-**			the actual size is retunred in the new_size parameter
+** 		The returned memory pointer must point to atleast new_size number of bytes (unless new_size is NULL or (a pointer to) 0)
+**			the actual size is returned in the new_size parameter
 **		The returned memory pointer may or may not point to the same old_memory
 **			and Cutlery algorithms and datastructures will not make any such assumptions about implementation details of the memory allocator provided to them
 **
 **		this function must return NULL on any error that you encounter,
 **		inclusive but not limited to 
 **			* memory allocation failure due to excessive memory utilization
+**
+**		success of a call to only deallocate (new_size = NULL or (a pointer to) 0) can not be known, it is assumed that such a deallocate call always succeeds
+**
+**
+**		BUT you can query if the old_memory memory is owned by the allocater, by setting new_size = old_size,
+**		where retrun value of NON-NULL (same as old_memory) implies ownership, and a NULL return implies that the old_memory is not owned by the corresponding allocator
 */
 typedef void* (*memory_allocator_function)(void* allocator_context, void* old_memory, cy_uint old_size, cy_uint* new_size, cy_uint new_alignment, memory_allocator_initialization initialization);
 
