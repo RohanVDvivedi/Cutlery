9eeb1a14d7d87b82d0f5ded87cd5df9ed9b8e06e
RohanVDvivedi <rohandvivedi@gmail.com>
2023-06-06 01:20:03 -0700

    n-ary tree as array util to use CY_UINT_C

--- inc/n-ary_tree_as_array_util.h
+++ inc/n-ary_tree_as_array_util.h
@@ -23,21 +23,21 @@
 	hence the last element in the tree is at index (CY_UINT_MAX - 1)
 */
 
-#define is_root_index_N(index)								((index) == 0)
+#define is_root_index_N(index)								((index) == CY_UINT_C(0))
 
 // a root in the N-ary tree will not have any parent
 #define has_parent_N(index)									(! is_root_index_N((index)))
 
-#define get_parent_index_N(child_index, N) 					(((child_index) - 1) / N)
+#define get_parent_index_N(child_index, N) 					(((child_index) - CY_UINT_C(1)) / N)
 
 // the parent_index has any children, only if the parent_index is lesser than or equal to the parent of the last index (CY_UINT_MAX - 1) of the tree
-#define can_have_any_children_N(parent_index, N)			(parent_index <= get_parent_index_N((CY_UINT_MAX - 1), N))
+#define can_have_any_children_N(parent_index, N)			(parent_index <= get_parent_index_N((CY_UINT_MAX - CY_UINT_C(1)), N))
 
 // here i must be in range [0, N) (0 inclusive, N exclusive)
-#define get_index_of_ith_child_N(parent_index, i, N)		((N * (parent_index)) + 1 + i)
+#define get_index_of_ith_child_N(parent_index, i, N)		((N * (parent_index)) + CY_UINT_C(1) + i)
 
 // below macro is only valid for binary trees
 // it can you used to get index of the sibling node (from left's index to right's index and viceversa)
-#define get_sibling_index_2(sibling_index)		((((sibling_index) + 1) ^ 1) - 1)
+#define get_sibling_index_2(sibling_index)		((((sibling_index) + CY_UINT_C(1)) ^ CY_UINT_C(1)) - CY_UINT_C(1))
 
 #endif
