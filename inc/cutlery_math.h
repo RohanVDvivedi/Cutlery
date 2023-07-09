516d0698bae2c8df01603dfce296eb1eac9fd6ae
RohanVDvivedi <rohandvivedi@gmail.com>
2023-07-08 21:23:33 -0700

    added macros that allow us to detect overflow

--- inc/cutlery_math.h
+++ inc/cutlery_math.h
@@ -5,4 +5,8 @@
 #define max(a,b) (((a)>(b))?(a):(b))
 #define compare(a,b) (((a)<(b))?(-1):(((a)>(b))?(1):(0)))
 
+// below 2 macros can be used to detect overflow on unsigned numeric data types on sum or multiplication, considering that A and B both are of the same type
+#define will_unsigned_sum_overflow(type_of_A_n_B, A, B)                ((B) > ((UNSIGNED_MAX_VALUE_OF(type_of_A_n_B)) - (A)))
+#define will_unsigned_mul_overflow(type_of_A_n_B, A, B) (((A) != 0) && ((B) > ((UNSIGNED_MAX_VALUE_OF(type_of_A_n_B)) / (A))))
+
 #endif
