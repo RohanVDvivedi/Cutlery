be8d8cc0a65c589f34cd3535be5be0dc2727f07f
RohanVDvivedi <rohandvivedi@gmail.com>
2023-07-12 07:27:04 -0700

    added comment to suggest the valid ness to parameter to an internal functins

--- src/arraylist.c
+++ src/arraylist.c
@@ -186,6 +186,8 @@ int set_nth_from_back_in_arraylist(arraylist* al, const void* data_p, cy_uint n)
 	return set_in_array(&(al->arraylist_holder), data_p, index_concerned);
 }
 
+// the below internal function assumes that
+// n_at is valid, and there are atleast non-zero number of element_count_to_remove number of elements after it
 int remove_elements_from_front_of_arraylist_at_INTERNAL(arraylist* al, cy_uint n_at, cy_uint element_count_to_remove)
 {
 	// TODO
