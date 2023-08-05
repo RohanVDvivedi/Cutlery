ac9bca5eaccdee0d59f8c7892cb736e326e7c0fe
RohanVDvivedi <rohandvivedi@gmail.com>
2023-08-04 22:53:06 -0700

    minor fix and inserting head circularly implemented

--- src/singlylist.c
+++ src/singlylist.c
@@ -59,35 +59,37 @@ const void* get_next_of_in_singlylist(const singlylist* sl, const void* data_xis
 	const slnode* node_xist = get_node(data_xist, sl);
 
 	// the data_xist must not be a free floating node
-	if(is_free_floating_slnode(sl, node_xist))
+	if(is_free_floating_slnode(node_xist))
 		return NULL;
 
 	return get_data(node_xist->next, sl);
 }
 
-// TODO from here --
-
 int insert_head_in_singlylist(singlylist* sl, const void* data)
 {
 	slnode* node_p = get_node(data, sl);
 
-	// insert only a new node to the singlylist
-	if(!is_new_slnode(sl, node_p))
+	// insert only a free floating slnode to the singlylist
+	if(!is_free_floating_slnode(node_p))
 		return 0;
 
 	if(is_empty_singlylist(sl))
 	{
 		sl->head = node_p;
 		sl->tail = node_p;
+		node_p->next = node_p;
 	}
 	else
 	{
 		node_p->next = sl->head;
+		sl->tail->next = node_p;
 		sl->head = node_p;
 	}
 
 	return 1;
 }
+
+// TODO from here --
 
 int insert_tail_in_singlylist(singlylist* sl, const void* data)
 {
