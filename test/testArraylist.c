3e874a936f85ab01ad38faad1a07f18ac76b5fea
RohanVDvivedi <rohandvivedi@gmail.com>
2023-08-06 23:36:15 -0700

    tested insert function, with element count = 0

--- test/testArraylist.c
+++ test/testArraylist.c
@@ -287,6 +287,30 @@ int main()
 	print_int_arraylist(al);
 	printf("\n\n");
 
+	// removing all
+	printf("REMOVING ALL\n");
+	remove_all_from_arraylist(al);
+	print_int_arraylist(al);
+	printf("\n\n");
+
+	operate_on_arraylist(al, INSERT_BACK);
+	print_int_arraylist(al);
+	printf("\n\n");
+
+	// removing all
+	printf("REMOVING ALL\n");
+	remove_all_from_arraylist(al);
+	print_int_arraylist(al);
+	printf("\n\n");
+
+	operate_on_arraylist(al, INSERT_FRONT);
+	print_int_arraylist(al);
+	printf("\n\n");
+
+	operate_on_arraylist(al, INSERT_FRONT);
+	print_int_arraylist(al);
+	printf("\n\n");
+
 	deinitialize_arraylist(al);
 	return 0;
 }
