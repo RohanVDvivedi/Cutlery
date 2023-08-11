1dce82fe4ba2829d7c0e1f5c7f59617c8ec2ccca
RohanVDvivedi <rohandvivedi@gmail.com>
2023-08-11 11:48:52 -0700

    implemented heap_sort for arraylist

--- src/arraylist_sort.c
+++ src/arraylist_sort.c
@@ -1,13 +1,11 @@
 #include<arraylist_sort.h>
 
 #include<heap.h>
+#include<circular_buffer_array_util.h>
 
-int heap_sort_arraylist(arraylist* al, cy_uint start_index, cy_uint last_index, int (*compare)(const void* data1, const void* data2));
-
-/*
-int heap_sort_array(array* array_p, cy_uint start_index, cy_uint last_index, int (*compare)(const void* data1, const void* data2))
+int heap_sort_arraylist(arraylist* al, cy_uint start_index, cy_uint last_index, int (*compare)(const void* data1, const void* data2))
 {
-	if(start_index > last_index || last_index >= get_capacity_array(array_p))
+	if(start_index > last_index || last_index >= get_element_count_arraylist(al))
 		return 0;
 
 	// compute the number of elements to sort; 0 or 1 number of elements do not need sorting
@@ -17,18 +15,19 @@ int heap_sort_array(array* array_p, cy_uint start_index, cy_uint last_index, int
 
 	// create a max heap that points to the array_p's contents that need to be sorted
 	heap sort_heap;
-	initialize_heap_with_memory(&sort_heap, total_elements, MAX_HEAP, BINARY_HEAP_DEGREE, compare, NO_HEAP_NODE_OFFSET, array_p->data_p_p + start_index);
-	sort_heap.element_count = total_elements;
+	initialize_heap_with_memory(&sort_heap, get_capacity_arraylist(al), MAX_HEAP, BINARY_HEAP_DEGREE, compare, NO_HEAP_NODE_OFFSET, al->arraylist_holder.data_p_p);
+	sort_heap.heap_holder.first_index = add_circularly(al->first_index, start_index, get_capacity_arraylist(al));
+	sort_heap.heap_holder.element_count = total_elements;
 
 	// now max heapify all elements that we need to sort
 	heapify_all(&sort_heap);
 
 	// place the top of the heap element in the array, then pop heap
-	for(cy_uint i = total_elements; i > 0; i--)
+	for(cy_uint i = total_elements; i > 1; i--)
 	{
 		const void* max_data = get_top_of_heap(&sort_heap);
 		pop_from_heap(&sort_heap);
-		set_in_array(array_p, max_data, start_index + i - 1);
+		set_in_arraylist(al, max_data, start_index + i - 1);
 	}
 
 	// deinitialize the sort_heap
@@ -36,4 +35,3 @@ int heap_sort_array(array* array_p, cy_uint start_index, cy_uint last_index, int
 
 	return 1;
 }
-*/
