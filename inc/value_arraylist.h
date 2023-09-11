f199e326ddb8230299f486f063ebcf4835b1a6e6
RohanVDvivedi <rohandvivedi@gmail.com>
2023-09-10 22:40:40 -0700

    sort functions specific to container work with only contained_type

--- inc/value_arraylist.h
+++ inc/value_arraylist.h
@@ -77,9 +77,9 @@ int remove_from_heap_ ## container(container* c, heap_info* hinfo, cy_uint degre
                                                                                                                                \
 /* container specific sorting functions */                                                                                     \
 /* (use these when index_accessed_search_sort sorting functions are restricted to only be used with arraylist) */              \
-int merge_sort_ ## container(container* c, cy_uint start_index, cy_uint last_index, int (*compare)(const void* data1, const void* data2), memory_allocator mem_allocator);\
-int heap_sort_ ## container(container* c, cy_uint start_index, cy_uint last_index, int (*compare)(const void* data1, const void* data2));\
-int radix_sort_ ## container(container* c, cy_uint start_index, cy_uint last_index, unsigned long long int (*get_sort_attribute)(const void* data), memory_allocator mem_allocator);\
+int merge_sort_ ## container(container* c, cy_uint start_index, cy_uint last_index, int (*compare)(const contained_type* data1, const contained_type* data2), memory_allocator mem_allocator);\
+int heap_sort_ ## container(container* c, cy_uint start_index, cy_uint last_index, int (*compare)(const contained_type* data1, const contained_type* data2));\
+int radix_sort_ ## container(container* c, cy_uint start_index, cy_uint last_index, unsigned long long int (*get_sort_attribute)(const contained_type* data), memory_allocator mem_allocator);\
                                                                                                                                \
 /* functions to increase decrease capacity of the container */                                                                 \
 int expand_ ## container(container* c);                                                                                        \
@@ -441,8 +441,8 @@ int remove_from_heap_ ## container(container* c, heap_info* hinfo, cy_uint degre
                                                                                                                                \
 /* container specific sorting functions */                                                                                     \
 /* (use these when index_accessed_search_sort sorting functions are restricted to only be used with arraylist) */              \
-int merge_sort_ ## container(container* c, cy_uint start_index, cy_uint last_index, int (*compare)(const void* data1, const void* data2), memory_allocator mem_allocator);\
-int heap_sort_ ## container(container* c, cy_uint start_index, cy_uint last_index, int (*compare)(const void* data1, const void* data2))\
+int merge_sort_ ## container(container* c, cy_uint start_index, cy_uint last_index, int (*compare)(const contained_type* data1, const contained_type* data2), memory_allocator mem_allocator);\
+int heap_sort_ ## container(container* c, cy_uint start_index, cy_uint last_index, int (*compare)(const contained_type* data1, const contained_type* data2))\
 {                                                                                                                              \
 	if(start_index > last_index || last_index >= get_element_count_ ## container(c))                                           \
 		return 0;                                                                                                              \
@@ -479,7 +479,7 @@ int heap_sort_ ## container(container* c, cy_uint start_index, cy_uint last_inde
                                                                                                                                \
 	return 1;                                                                                                                  \
 }                                                                                                                              \
-int radix_sort_ ## container(container* c, cy_uint start_index, cy_uint last_index, unsigned long long int (*get_sort_attribute)(const void* data), memory_allocator mem_allocator)\
+int radix_sort_ ## container(container* c, cy_uint start_index, cy_uint last_index, unsigned long long int (*get_sort_attribute)(const contained_type* data), memory_allocator mem_allocator)\
 {                                                                                                                              \
 	if(mem_allocator == NULL || start_index > last_index || last_index >= get_element_count_ ## container(c))                  \
 		return 0;                                                                                                              \
