4fbd6e76463550fd15fb714b077753ccef4491ea
RohanVDvivedi <rohandvivedi@gmail.com>
2023-09-12 00:15:16 -0700

    compare numbers has been renamed to avoid comflict with comparator function

--- inc/cutlery_math.h
+++ inc/cutlery_math.h
@@ -5,7 +5,7 @@
 
 #define min(a,b) (((a)<(b))?(a):(b))
 #define max(a,b) (((a)>(b))?(a):(b))
-#define compare(a,b) (((a)<(b))?(-1):(((a)>(b))?(1):(0)))
+#define compare_numbers(a,b) (((a)<(b))?(-1):(((a)>(b))?(1):(0)))
 
 // below 2 macros can be used to detect overflow on unsigned numeric data types on sum or multiplication, considering that A and B both are of the same type
 #define will_unsigned_sum_overflow(type_of_A_n_B, A, B)                ((B) > ((UNSIGNED_MAX_VALUE_OF(type_of_A_n_B)) - (A)))
