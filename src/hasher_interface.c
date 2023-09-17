40eb0f200d16f3258fcecc824efa56c9a03fd79a
RohanVDvivedi <rohandvivedi@gmail.com>
2023-09-16 19:58:04 -0700

    hashing will always returns hash as cy_uint

--- src/hasher_interface.c
+++ src/hasher_interface.c
@@ -1,6 +1,6 @@
 #include<hasher_interface.h>
 
-int hash_with_hasher(const hasher_interface* hasher, const void* data)
+cy_uint hash_with_hasher(const hasher_interface* hasher, const void* data)
 {
 	if(hasher->context == NULL)
 		return hasher->hash1(data);
