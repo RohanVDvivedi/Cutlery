7000e1eee4d1032d80f13605c930dda3d46df74c
RohanVDvivedi <rohandvivedi@gmail.com>
2023-09-16 19:35:10 -0700

    renamed header files of all interfaces and added logic for comparator and hasher interface

---
+++ inc/interface_hasher.h
@@ -0,0 +1,33 @@
+#ifndef INTERFACE_HASHER_H
+#define INTERFACE_HASHER_H
+
+/* 
+** A hasher interface is an interface allowing you to hash data (pointed to by data pointers)
+** It allows you to hash with OR without a context
+*/
+
+typedef struct hasher_interface hasher_interface;
+struct hasher_interface
+{
+	// context for the hasher
+	const void* context;
+
+	// the hash functions of the hasher
+	union
+	{
+		// hash function to use when context == NULL
+		int (*hash1)(const void* data);
+
+		// hash function to use when context != NULL
+		int (*hash2)(const void* context, const void* data);
+	};
+};
+
+// you must use only this function, with the hasher, to avail the complete functionality of the hasher
+int hash_with_hasher(const hasher_interface* hasher, const void* data);
+
+// You may use the macros below to initialize your custom hasher_interface
+#define simple_hasher(hash_function_p)               ((const hasher_interface){.context = NULL,      .hasher1 = hash_function_p})
+#define contexted_hasher(context_p, hash_function_p) ((const hasher_interface){.context = context_p, .hasher2 = hash_function_p})
+
+#endif
