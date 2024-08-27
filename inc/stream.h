6eda1242f9819bdf6af00316371b76ca1df6f1ad
RohanVDvivedi <rohandvivedi@gmail.com>
2024-08-27 21:50:13 +0530

    I will not define a default for you

--- inc/stream.h
+++ inc/stream.h
@@ -60,8 +60,6 @@ int initialize_stream(
 						cy_uint max_unflushed_bytes_count
 					);
 
-#define DEFAULT_MAX_UNFLUSHED_BYTES_COUNT 4096
-
 int is_readable_stream(stream* strm);
 
 int is_writable_stream(stream* strm);
