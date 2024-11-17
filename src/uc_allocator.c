85bde715c66b355406aa922bb42bc5cd72167b84
RohanVDvivedi <rohandvivedi@gmail.com>
2024-11-17 23:43:25 +0530

    minor error

--- src/uc_allocator.c
+++ src/uc_allocator.c
@@ -10,4 +10,4 @@ static void initialize_free_block(free_block* b);
 static free_block* carve_out_free_block_in_memory_region(void* memory, cy_uint size);
 
 // function to be used for contexted comparator for free_blocks bst
-static int compare_by_sizes_for_free_blocks_bst(const uc_allocator_context* ucac_p, const free_block* b1, const free* b2);
+static int compare_by_sizes_for_free_blocks_bst(const uc_allocator_context* ucac_p, const free_block* b1, const free_block* b2);
