acfa383316017db4443629cf7535aa4a942ca6c5
RohanVDvivedi <rohandvivedi@gmail.com>
2024-11-18 11:22:09 +0530

    added logic to initialize blocks

--- src/uc_allocator.c
+++ src/uc_allocator.c
@@ -1,10 +1,19 @@
 #include<uc_allocator.h>
 
 // only default initializes the any_block struct
-static void initialize_any_block(any_block* b);
+static void initialize_any_block(any_block* b, int is_free)
+{
+	b->is_free = is_free;
+	b->is_marked = 0;
+	initialize_llnode(&(b->ab_node));
+}
 
 // only default initializes the free_block struct
-static void initialize_free_block(free_block* b);
+static void initialize_free_block(free_block* b)
+{
+	initialize_any_block(&(b->any), 1); // marked is_free = 1
+	initialize_bstnode(&(b->fb_node));
+}
 
 // if it returns NULL, then the alignment or space constraints do not allow this
 static free_block* carve_out_free_block_in_memory_region(void* memory, cy_uint size);
