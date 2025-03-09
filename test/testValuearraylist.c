e14d618ecac8971e17631d02cf90c77c8360cc2b
RohanVDvivedi <rohandvivedi@gmail.com>
2025-03-09 13:34:47 +0530

    changes according to the new api

--- test/testValuearraylist.c
+++ test/testValuearraylist.c
@@ -3,9 +3,9 @@
 #include<stdio.h>
 
 data_definitions_value_arraylist(int_list, int)
-declarations_value_arraylist(int_list, int)
+declarations_value_arraylist(int_list, int, )
 #define EXPANSION_FACTOR 1.5
-function_definitions_value_arraylist(int_list, int)
+function_definitions_value_arraylist(int_list, int, )
 
 int compare_ints(const void* a, const void* b)
 {
