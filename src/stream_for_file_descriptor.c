fe2ee7ca5ba893106ad5358e6dc2479441845043
RohanVDvivedi <rohandvivedi@gmail.com>
2025-03-21 22:13:02 +0530

    minor change small leap into avoiding small redundant allocations

--- src/stream_for_file_descriptor.c
+++ src/stream_for_file_descriptor.c
@@ -10,9 +10,16 @@
 // must always be lesser than or equal to SSIZE_MAX
 #define MAX_IO_SIZE (SSIZE_MAX >> 4)
 
+/*
+	I regret making this change of storing int inside a void*
+	I wanted to just save some malloc calls in this stream, avoid such small allocations to avoid fragmentation and also extra computation
+	But I strongly believe that void* should generally be atleast larger than an integer on most systems
+	Intermediate typecasts to `long long int` is just to keep the compiler happy, regardless of -Wall
+*/
+
 static cy_uint read_from_fd(void* stream_context, void* data, cy_uint data_size, int* error)
 {
-	ssize_t ret = read(*((int*)stream_context), data, min(data_size, MAX_IO_SIZE));
+	ssize_t ret = read((long long int)stream_context, data, min(data_size, MAX_IO_SIZE));
 	if(ret == -1)
 	{
 		*error = errno;
@@ -23,7 +30,7 @@ static cy_uint read_from_fd(void* stream_context, void* data, cy_uint data_size,
 
 static cy_uint write_to_fd(void* stream_context, const void* data, cy_uint data_size, int* error)
 {
-	ssize_t ret = write(*((int*)stream_context), data, min(data_size, MAX_IO_SIZE));
+	ssize_t ret = write((long long int)stream_context, data, min(data_size, MAX_IO_SIZE));
 	if(ret == -1)
 	{
 		*error = errno;
@@ -34,26 +41,21 @@ static cy_uint write_to_fd(void* stream_context, const void* data, cy_uint data_
 
 static void close_stream_context_fd(void* stream_context, int* error)
 {
-	if(close(*((int*)stream_context)) == -1)
+	if(close((long long int)stream_context) == -1)
 		*error = errno;
 }
 
 static void destroy_stream_context_fd(void* stream_context)
 {
-	free(stream_context);
+	// NOP
 }
 
 int initialize_stream_for_fd(stream* strm, int fd)
 {
-	int* stream_context = malloc(sizeof(int));
-	if(stream_context == NULL)
-		return 0;
-	*stream_context = fd;
-
-	if(!initialize_stream(strm, stream_context, read_from_fd, write_to_fd, close_stream_context_fd, destroy_stream_context_fd, NULL, DEFAULT_MAX_UNFLUSHED_BYTES_COUNT))
+	if(!initialize_stream(strm, (void*)((long long int)fd), read_from_fd, write_to_fd, close_stream_context_fd, destroy_stream_context_fd, NULL, DEFAULT_MAX_UNFLUSHED_BYTES_COUNT))
 	{
 		// since we never opened this stream_context (i.e. the fd), so we do not close it
-		destroy_stream_context_fd(stream_context);
+		destroy_stream_context_fd((void*)((long long int)fd));
 		return 0;
 	}
 
