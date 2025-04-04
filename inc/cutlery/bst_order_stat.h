580f2eab16683ceea245933a9af1edf61792626a
RohanVDvivedi <rohandvivedi@gmail.com>
2025-04-04 10:10:07 +0530

    function names changed to reflect the variable names

--- inc/cutlery/bst_order_stat.h
+++ inc/cutlery/bst_order_stat.h
@@ -6,10 +6,10 @@
 // below two functions take bstnode as input but actually work with order_stat_bstnode
 
 // returns the sub_tree_size as present in the order_stat_bstnode of the node_p
-cy_uint get_sub_tree_size(const bst* bst_p, const bstnode* node_p);
+cy_uint get_subtree_size(const bst* bst_p, const bstnode* node_p);
 
 // below functions recomputes the sub_tree_size of all nodes from node_p to the most parent node that is wrong/incorrect
 // it force recomputes the sub_tree_size at node_p
-cy_uint recompute_sub_tree_size(const bst* bst_p, bstnode* node_p);
+cy_uint recompute_subtree_size(const bst* bst_p, bstnode* node_p);
 
 #endif
--- src/bst_order_stat.c
+++ src/bst_order_stat.c
@@ -2,21 +2,21 @@
 
 #include<cutlery/cutlery_node.h>
 
-static cy_uint get_sub_tree_size_internal(const bst* bst_p, const bstnode* node_p)
+static cy_uint get_subtree_size_internal(const bst* bst_p, const bstnode* node_p)
 {
 	const void* data = get_data(node_p, bst_p);
 	const order_stat_bstnode* osb = data + bst_p->order_stat_node_offset;
 	return osb->subtree_size;
 }
 
-static void set_sub_tree_size_internal(const bst* bst_p, bstnode* node_p, cy_uint subtree_size)
+static void set_subtree_size_internal(const bst* bst_p, bstnode* node_p, cy_uint subtree_size)
 {
 	const void* data = get_data(node_p, bst_p);
 	order_stat_bstnode* osb = (order_stat_bstnode*)(data + bst_p->order_stat_node_offset);
 	osb->subtree_size = subtree_size;
 }
 
-cy_uint get_sub_tree_size(const bst* bst_p, const bstnode* node_p)
+cy_uint get_subtree_size(const bst* bst_p, const bstnode* node_p)
 {
 	if(!is_order_stat_bst(bst_p))
 		return INVALID_INDEX;
@@ -25,10 +25,10 @@ cy_uint get_sub_tree_size(const bst* bst_p, const bstnode* node_p)
 	if(node_p == NULL)
 		return 0;
 
-	return get_sub_tree_size_internal(bst_p, node_p);
+	return get_subtree_size_internal(bst_p, node_p);
 }
 
-cy_uint recompute_sub_tree_size(const bst* bst_p, bstnode* node_p)
+cy_uint recompute_subtree_size(const bst* bst_p, bstnode* node_p)
 {
 	if(!is_order_stat_bst(bst_p))
 		return INVALID_INDEX;
@@ -38,21 +38,21 @@ cy_uint recompute_sub_tree_size(const bst* bst_p, bstnode* node_p)
 		return 0;
 
 	// force recompute the subtree size at the node_p
-	set_sub_tree_size_internal(bst_p, node_p, 0);
+	set_subtree_size_internal(bst_p, node_p, 0);
 
 	bstnode* curr_p = node_p;
 	while(curr_p != NULL)
 	{
-		cy_uint subtree_size = get_sub_tree_size_internal(bst_p, curr_p);
-		cy_uint computed_subtree_size = get_sub_tree_size_internal(bst_p, curr_p) + 1 + get_sub_tree_size_internal(bst_p, curr_p);
+		cy_uint subtree_size = get_subtree_size_internal(bst_p, curr_p);
+		cy_uint computed_subtree_size = get_subtree_size_internal(bst_p, curr_p) + 1 + get_subtree_size_internal(bst_p, curr_p);
 
 		if(subtree_size == computed_subtree_size)
 			break;
 
-		set_sub_tree_size_internal(bst_p, curr_p, computed_subtree_size);
+		set_subtree_size_internal(bst_p, curr_p, computed_subtree_size);
 
 		curr_p = curr_p->parent;
 	}
 
-	return get_sub_tree_size_internal(bst_p, node_p);
+	return get_subtree_size_internal(bst_p, node_p);
 }
