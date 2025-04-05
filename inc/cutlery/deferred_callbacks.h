f416cecba53ef3ded1a317b3e639052e44117c0e
RohanVDvivedi <rohandvivedi@gmail.com>
2025-04-05 17:15:17 +0530

    deferred_callbacks added

---
+++ inc/cutlery/deferred_callbacks.h
@@ -0,0 +1,25 @@
+#ifndef DEFERRED_CALLBACKS_H
+#define DEFERRED_CALLBACKS_H
+
+typedef struct deferred_callback deferred_callback;
+struct deferred_callback
+{
+	void (*deferred_callback_func)(void* resource_p);
+	void* resource_p;
+};
+
+#define NEW_DEFERED_CALLS(CAPACITY) \
+deferred_callback deferrence_callbacks[CAPACITY] = {}; \
+cy_uint deferrence_callbacks_size = 0;
+
+#define DEFER(deferred_callback_func_v, resource_p_v) \
+deferrence_callbacks[deferrence_callbacks_size++] = (deferred_callback){deferred_callback_func_v, resource_p_v};
+
+#define CALL_ALL_DEFERRED() \
+while(deferrence_callbacks_size > 0) \
+{ \
+	deferred_callback dc = deferrence_callbacks[--deferrence_callbacks_size]; \
+	dc.deferred_callback_func(dc.resource_p); \
+}
+
+#endif
