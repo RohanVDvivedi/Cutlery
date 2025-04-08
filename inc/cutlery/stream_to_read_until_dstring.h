351d703f4c956eb756665068fac4dae92b50d2c8
RohanVDvivedi <rohandvivedi@gmail.com>
2025-04-08 15:06:01 +0530

    added comment to mitigate and keep api simple

--- inc/cutlery/stream_to_read_until_dstring.h
+++ inc/cutlery/stream_to_read_until_dstring.h
@@ -9,6 +9,19 @@
 // after which it remains in closed state
 // the given read_until_dstr is consumed from the underlying stream, but it does not get produced
 
+/*
+	This stream relies on malloc/realloc/calloc and free for its internal structures, so it is not fit for some baremetal embedded platform
+	It does not just point to your provided input patamaters but instead makes a deep-copy of the read_until_dstr, read_until_dstr_spml
+	And also heap allocates its internal dpipe structure (that preserves the characters read up-until now, but those have not been produced yet)
+
+	All-In-All, if you don't have malloc/free on your system, then either avoid using this stream OR write your own version of initialize_stream_for_reading_until_dstring
+	such that
+	  * it either takes in cutlery specific memory_allocator
+	  * OR points to input parameters provided by you (and then you need to release these resources when this stream is closed)
+	  * OR design it such that it takes your input parameters and takes full ownership to destroy them upon exit
+	** all of the above three options would make this stream api too complicated and hence are not provided
+*/
+
 typedef struct read_until_dstring_stream_context read_until_dstring_stream_context;
 struct read_until_dstring_stream_context
 {
