3680a9962879ee0f4c7710bda716e337c525a09c
RohanVDvivedi <rohandvivedi@gmail.com>
2025-04-12 23:12:54 +0530

    minor comment fixed

--- inc/cutlery/cutlery_node.h
+++ inc/cutlery/cutlery_node.h
@@ -3,10 +3,11 @@
 
 /*
 *	MACROS TO CONVERT FROM DATA TO NODE AND NODE TO DATA
-*  This is only used by embedded node based data structures.
-*  All embedded node based data structures (like linkedlist, singlylist, bst and heap)
-*  store node_offset to their nodes.
-*  Since, their nodes (that these datastructures operate on) are always at this fixed offset inside the user's data (structs).
+*  This is only used by embedded node based data structures (intrusive data structures),
+*  that includes, all embedded node based data structures (like linkedlist, singlylist, bst, hashmap, heap and union_find)
+*  which store node_offset to their nodes.
+*  Since, their nodes (that these datastructures operate on) are always at this fixed offset inside the user's data (structs),
+*  simple pointer arithmetic as described in below macros to go from (user's) data pointer to node pointers, and viceversa, suffices.
 */
 #define get_data(node_p, ds_p) 	(((const void*)(node_p)) - ((ds_p)->node_offset))
 #define get_node(data_p, ds_p) 	(((void*)(data_p)) + ((ds_p)->node_offset))
