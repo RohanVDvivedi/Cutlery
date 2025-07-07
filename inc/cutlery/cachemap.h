bb186c9b69cea9b72c72ba5c76d8aa8ae9864926
RohanVDvivedi <rohandvivedi@gmail.com>
2025-07-07 22:40:26 +0530

    fixed comments

--- inc/cutlery/cachemap.h
+++ inc/cutlery/cachemap.h
@@ -8,12 +8,12 @@
 /*
 	This is an LRU cache, why not LFU, MRU or some custom priority based cache, you say
 	MRU -> too specific use case, only works for multiple scan over scan of objects
-	LFU -> saturating counters, O(log(n)) access due to use of heap, and inabilit to evict highly used objects from past
+	LFU -> saturating counters, O(log(n)) access due to use of heap, and inability to evict highly used objects from past, slow to adapt to changing workloads
 	priority based -> too much complexity, and no temporal locality used
 
-	This is a very simple cachemap implementation, which is also a directive on how this library can be used to make more complex datastructures
+	This is a very simple LRU cache implementation, which is also a directive on how this library can be used to make more complex datastructures
 
-	UNLIKE OTHER MAP LIKE CUTLERY DATA STRUCTURES INSERTS WILL FAIL ON DUPLICATE INSERTIONS
+	UNLIKE OTHER MAP LIKE CUTLERY DATA STRUCTURES (hashmap and bst) INSERTS WILL FAIL ON DUPLICATE INSERTIONS
 */
 
 typedef struct cchnode cchnode;
